c6aa856457a097bfa302f919928ae36f2fc65457
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2014-03-13 17:21:31 -0400

    Fix labels on gate plot

--- PlotChannel2.py
+++ PlotChannel2.py
@@ -12,11 +12,11 @@ def plot_gate_params(chan,plotpow):
         varray=np.linspace(VMIN,VMAX,len(ma))
         subplot(211)
         plt.title(chan.path)
-        plt.plot(varray,1e3/mb, label='tau_m')
+        plt.plot(varray,1e3/mb, label='m ' + chan.path)
         plt.ylabel('tau, ms')
         if chan.Ypower > 0:
-            plt.plot(varray,1e3/hb, label='tau_h')
-        plt.legend()
+            plt.plot(varray,1e3/hb, label='h ' + chan.path)
+        plt.legend(loc='best', fontsize=8)
         #
         subplot(212)
         if plotpow:
@@ -24,11 +24,11 @@ def plot_gate_params(chan,plotpow):
             inf=(ma/mb)**chan.Xpower
         else:
             inf=(ma/mb)
-        plt.plot(varray,inf, label='m_inf')
+        plt.plot(varray,inf, label='m ' + chan.path)
         plt.axis([-0.12,0.05,0,1])
         if chan.Ypower > 0:
-            plt.plot(varray,ha/hb, label='h_inf')
-        plt.legend()
+            plt.plot(varray,ha/hb, label='h ' + chan.path)
+        plt.legend(loc='best', fontsize=8)
         plt.show()
     #
 
