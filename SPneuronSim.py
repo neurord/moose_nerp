64826454c0fb876e6778f3d96cfd978ef070bdc7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-07-20 17:48:09 -0400

    Fix references to param_sim

--- SPneuronSim.py
+++ SPneuronSim.py
@@ -25,8 +25,7 @@ from spspine import (cell_proto,
                      neuron_graph,
                      test_plas,
                      util as _util)
-import param_sim as sim
-from spspine import param_cond
+from spspine import param_cond, param_sim
 
 try:
     from ParamOverrides import *
@@ -36,28 +35,28 @@ except ImportError:
 #################################-----------create the model
 ##create 2 neuron prototypes, optionally with synapses, calcium, and spines
 
-MSNsyn,neuron,capools,synarray,spineHeads = cell_proto.neuronclasses(sim.plotchan,sim.plotpow,sim.calcium,sim.synYesNo,sim.spineYesNo,sim.ghkYesNo)
+MSNsyn,neuron,capools,synarray,spineHeads = cell_proto.neuronclasses(param_sim.plotchan,param_sim.plotpow,param_sim.calcium,param_sim.synYesNo,param_sim.spineYesNo,param_sim.ghkYesNo)
 
 #If calcium and synapses created, could test plasticity at a single synapse in syncomp
-if sim.synYesNo:
-    syn,plas,stimtab=test_plas.test_plas(sim.syncomp,sim.calcium,sim.plasYesNo,sim.inpath,MSNsyn)
+if param_sim.synYesNo:
+    syn,plas,stimtab=test_plas.test_plas(param_sim.syncomp,param_sim.calcium,param_sim.plasYesNo,param_sim.inpath,MSNsyn)
 else:
     syn,plas = {}, {}
 
 ####---------------Current Injection
-currents = _util.inclusive_range(sim.current1,sim.current2,sim.currinc)
-pg=inject_func.setupinj(sim.delay,sim.width,neuron)
+currents = _util.inclusive_range(param_sim.current1,param_sim.current2,param_sim.currinc)
+pg=inject_func.setupinj(param_sim.delay,param_sim.width,neuron)
 
 ###############--------------output elements
 data = moose.Neutral('/data')
 
-vmtab,catab,plastab,currtab = neuron_graph.graphtables(neuron,sim.plotcurr,sim.currmsg,capools,plas,syn)
+vmtab,catab,plastab,currtab = neuron_graph.graphtables(neuron,param_sim.plotcurr,param_sim.currmsg,capools,plas,syn)
 #if sim.spineYesNo:
 #    spinecatab,spinevmtab=spinetabs()
 
 ########## clocks are critical
 simpaths=['/'+neurotype for neurotype in param_cond.neurontypes()]
-clocks.assign_clocks(simpaths, '/data', sim.simdt, sim.plotdt, sim.hsolve)
+clocks.assign_clocks(simpaths, '/data', param_sim.simdt, param_sim.plotdt, param_sim.hsolve)
 
 ###########Actually run the simulation
 def run_simulation(injection_current, simtime):
@@ -69,10 +68,10 @@ def run_simulation(injection_current, simtime):
 if __name__ == '__main__':
     Alltraces=[]
     for inj in currents:
-        run_simulation(injection_current=inj, simtime=sim.simtime)
-        neuron_graph.graphs(vmtab,sim.plotcurr,currtab,sim.currlabel,catab,plastab)
+        run_simulation(injection_current=inj, simtime=param_sim.simtime)
+        neuron_graph.graphs(vmtab,param_sim.plotcurr,currtab,param_sim.currlabel,catab,plastab)
         Alltraces.append(vmtab[0][0].vector)
-        #if sim.spineYesNo:
+        #if param_sim.spineYesNo:
         #    spineFig(spinecatab,spinevmtab)
     neuron_graph.SingleGraphSet(Alltraces,currents)
 
