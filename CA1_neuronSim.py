c354886ac5d02d36d87f011be50c31d5f5477c02
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-09-15 23:10:12 -0400

    ca1: rename CA1 to ca1
    The top-level script is not renamed, because I want to get rid of it
    entirely.

--- CA1_neuronSim.py
+++ CA1_neuronSim.py
@@ -28,7 +28,7 @@ from spspine import (cell_proto,
                      logutil,
                      util,
                      standard_options)
-from spspine import CA1
+from spspine import ca1
 from spspine.graph import plot_channel, neuron_graph
 
 option_parser = standard_options.standard_options(default_injection_current=[50e-12, 100e-12])
@@ -40,35 +40,35 @@ log = logutil.Logger()
 #################################-----------create the model
 ##create 2 neuron prototypes, optionally with synapses, calcium, and spines
 
-MSNsyn,neuron,capools,synarray,spineHeads = cell_proto.neuronclasses(CA1)
+MSNsyn,neuron,capools,synarray,spineHeads = cell_proto.neuronclasses(ca1)
 
 #If calcium and synapses created, could test plasticity at a single synapse in syncomp
-if CA1.synYN:
-    syn,plas,stimtab=plastic_synapse.plastic_synapse(CA1, param_sim.syncomp, MSNsyn, param_sim.stimtimes)
+if ca1.synYN:
+    syn,plas,stimtab=plastic_synapse.plastic_synapse(ca1, param_sim.syncomp, MSNsyn, param_sim.stimtimes)
 else:
     syn,plas = {}, {}
 
 ####---------------Current Injection
 
-pg=inject_func.setupinj(CA1, param_sim.injection_delay, param_sim.injection_width, neuron)
+pg=inject_func.setupinj(ca1, param_sim.injection_delay, param_sim.injection_width, neuron)
 
 ###############--------------output elements
 if param_sim.plot_channels:
-    for chan in CA1.Channels.keys():
+    for chan in ca1.Channels.keys():
         libchan=moose.element('/library/'+chan)
         plot_channel.plot_gate_params(libchan,param_sim.plot_activation,
-                                      CA1.VMIN, CA1.VMAX, CA1.CAMIN, CA1.CAMAX)
+                                      ca1.VMIN, ca1.VMAX, ca1.CAMIN, ca1.CAMAX)
 
 data = moose.Neutral('/data')
 
-vmtab,catab,plastab,currtab = tables.graphtables(CA1, neuron,
+vmtab,catab,plastab,currtab = tables.graphtables(ca1, neuron,
                                                  param_sim.plot_current,
                                                  param_sim.plot_current_message,
                                                  capools,plas,syn)
 #if sim.spineYesNo:
 #    spinecatab,spinevmtab=spinetabs()
 ########## clocks are critical. assign_clocks also sets up the hsolver
-simpaths=['/'+neurotype for neurotype in CA1.neurontypes()]
+simpaths=['/'+neurotype for neurotype in ca1.neurontypes()]
 clocks.assign_clocks(simpaths, '/data', param_sim.simdt, param_sim.plotdt, param_sim.hsolve)
 
 ###########Actually run the simulation
@@ -82,11 +82,11 @@ if __name__ == '__main__':
     traces, names = [], []
     for inj in param_sim.injection_current:
         run_simulation(injection_current=inj, simtime=param_sim.simtime)
-        neuron_graph.graphs(CA1, vmtab, param_sim.plot_current, param_sim.simtime,
+        neuron_graph.graphs(ca1, vmtab, param_sim.plot_current, param_sim.simtime,
                             currtab,param_sim.plot_current_label, catab, plastab)
         traces.append(vmtab[0][0].vector)
-        names.append('CA1 @ {}'.format(inj))
-        #if CA1.spineYN:
+        names.append('ca1 @ {}'.format(inj))
+        #if ca1.spineYN:
         #    spineFig(spinecatab,spinevmtab)
     neuron_graph.SingleGraphSet(traces, names, param_sim.simtime)
 
