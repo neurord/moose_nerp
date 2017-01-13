d92ec93dc460e7e8fedcdd41191dae1d470038b0
skirolik <saumitra.kirolikar@gmail.com>
2017-01-13 17:53:47 -0500

    gp: syn param

--- spspine/gp/param_syn.py
+++ spspine/gp/param_syn.py
@@ -18,21 +18,21 @@ _NMDA_MgParams = MgParams(A = 1/6.0,
                            C = 1.4)
 
 #Sriram uses 0.109e-9 for AMPA and 0.9e-9 for Gaba
-_SynGaba = SynChannelParams(Erev = -60e-3,
-                             tau1 = 0.25e-3,
-                             tau2 = 3.75e-3,
-                             Gbar = 0.2e-9,
+_SynGaba = SynChannelParams(Erev = -80e-3,
+                             tau1 = 1e-3,
+                             tau2 = 12e-3,
+                             Gbar = 0.25e-9,
                              var=0.05)
-_SynAMPA = SynChannelParams(Erev = 5e-3,
-                             tau1 = 1.1e-3,
-                             tau2 = 5.75e-3,
-                             Gbar = 2e-9,
+_SynAMPA = SynChannelParams(Erev = 0,
+                             tau1 = 1e-3,
+                             tau2 = 3e-3,
+                             Gbar = 0.25e-9,
                              var=0.05,
                              spinic = True)
-_SynNMDA = SynChannelParams(Erev = 5e-3,
-                             tau1 = 1.1e-3,
-                             tau2 = 37.5e-3,
-                             Gbar = 2e-9,
+_SynNMDA = SynChannelParams(Erev = 0,
+                             tau1 = 10e-3,
+                             tau2 = 30e-3,
+                             Gbar = 0.252e-9,
                              var=0.05,
                              MgBlock = _NMDA_MgParams,
                              spinic = True,
@@ -51,9 +51,14 @@ SYNAPSE_TYPES = NamedDict(
     nmda = _SynNMDA,
 )
 
-# number of synapses at each distance
-_gaba = {param_cond.prox:3, param_cond.dist:1} # param_cond.med:2,
-_ampa= {param_cond.prox:1, param_cond.dist:3}  # param_cond.med:2,
+#These will be used by synconn in connect.py, since AMPA and NMDA synapses usually go together
+#for same reason, the next lines only list ampa and gaba, and nmda are created the same as ampa
+NAME_AMPA='ampa'
+NAME_NMDA='nmda'
 
-NumSyn={'Gaba':_gaba,
+# number of synapses at each distance
+_gaba = {param_cond.prox:3, param_cond.dist:1}
+_ampa= {param_cond.prox:1, param_cond.dist:3}
+
+NumSyn={'gaba':_gaba,
         'ampa':_ampa}
