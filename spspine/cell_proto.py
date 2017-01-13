b9f76b19191e25387125d8dee596af082a42bbfd
skirolik <saumitra.kirolikar@gmail.com>
2017-01-13 17:47:19 -0500

    channel components for spherical soma

--- spspine/cell_proto.py
+++ spspine/cell_proto.py
@@ -21,6 +21,7 @@ def addOneChan(chanpath,gbar,comp,ghkYN, ghk=None, calciumPermeable=False):
     SA=np.pi*length*diam
     if length==0:
         SA=np.pi*diam**2
+        log.info('Check RA for spherical compartment',comp.name)
     proto = moose.element('/library/'+chanpath)
     chan = moose.copy(proto, comp, chanpath)[0]
     chan.Gbar = gbar * SA
