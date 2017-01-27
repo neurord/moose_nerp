89a726451cac6baaa0612f968e6447ffbac53fc1
Asia Jedrzejewska-Szmek <jjedrzej@gmu.edu>
2017-01-27 16:48:41 -0500

    Automatic choice of proper buffers/dyes (transfer from SP12)

--- spspine/d1d2/param_ca_plas.py
+++ spspine/d1d2/param_ca_plas.py
@@ -1,7 +1,8 @@
 #if calcium=0, then calcium pools not implemented
 #if calcium=2, then diffusion,buffers and pumps implemented (eventually)
 caltype = 1
-
+CDIYesNo = 1
+cadye = 0
 BufferParams = NamedList('BufferParams','''
 Name
 bTotal
@@ -35,7 +36,7 @@ calbindin = BufferParams('Calbindin', bTotal=80e-3, kf=0.028e6, kb=19.6, D=66e-1
 camc = BufferParams('CaMC', bTotal=15e-3, kf=0.006e6, kb=9.1, D=66.0e-12)
 camn = BufferParams('CaMN', bTotal=15e-3, kf=0.1e6, kb=1000., D=66.0e-12)
 fixed_buffer = BufferParams('Fixed_Buffer', bTotal=1, kf=0.4e6, kb=20e3, D=0)
-fura2 = BufferParams('Fura-2', bTotal=100e-3, kf=1000e3, kb=185, D=6e-11) #Kerrs
+Fura2 = BufferParams('Fura-2', bTotal=100e-3, kf=1000e3, kb=185, D=6e-11) #Kerrs
 Fluo5f_Wickens = BufferParams('Fluo5f_Wickens', bTotal=300.0e-3, kf=2.36e5, kb=82.6, D=6e-11)
 Fluo5f_Lovinger = BufferParams('Fluo5f_Lovinger', bTotal=300.0e-3, kf=2.36e5, kb=82.6, D=6e-11)
 Fluo4 = BufferParams('Fluo4', bTotal=100.0e-3, kf=2.36e5, kb=82.6, D=6e-11)
@@ -45,4 +46,14 @@ MMpump_soma = PumpParams('MMpump_soma',Km=0.3e-3,Kcat=85e-8)
 MMpump_dend = PumpParams('MMpump_dend',Km=0.3e-3,Kcat=8e-8)
 NCX = PumpParams("NCX",Km=1e-3,Kcat=0)
 
+BufferCombinations = {
+    0: [calbindin,camn,camc,fixed_buffer],
+    1: [Fura2,fixed_buffer],
+    2: [Fluo5f_Wickens,fixed_buffer],
+    3: [Fluo4,fixed_buffer],
+    4: [Fluo4FF,fixed_buffer],
+    5: [Fluo5f_Lovinger,fixed_buffer]
 
+}
+    
+ModelBuffers =  BufferCombinations[cadye]                    
