4ff500624b13ee780b11debd887a32302f25997b
Asia Jedrzejewska-Szmek <jjedrzej@gmu.edu>
2017-03-27 15:21:25 -0400

    Rewrite plasticity

--- spspine/spines.py
+++ spspine/spines.py
@@ -1,8 +1,11 @@
 from __future__ import print_function, division
 import numpy as np
 import moose
-
+import random
+random.seed(1)
 from . import logutil
+from util import distance_mapping
+from spspine.add_channel import addOneChan
 log = logutil.Logger()
 
 #NAME_NECK and NAME_HEAD are used in calcium.py to add calcium objects to spines
@@ -10,24 +13,24 @@ log = logutil.Logger()
 NAME_NECK = "neck"
 NAME_HEAD = "head"
 
-def setSpineCompParams(model, comp,compdia,complen):
-    comp.diameter=compdia
-    comp.length=complen
-    XArea=np.pi*compdia*compdia/4
-    circumf=np.pi*compdia
+def setSpineCompParams(model, comp,compdia,complen,RA,RM,CM):
+    comp.diameter = compdia
+    comp.length = complen
+    XArea = np.pi*compdia*compdia/4
+    circumf = np.pi*compdia
     log.debug('Xarea,circumf of {}, {}, {} CM {} {}',
               comp.path, XArea, circumf,
-              model.SpineParams.spineCM*complen*circumf)
-    comp.Ra = model.SpineParams.spineRA*complen/XArea
-    comp.Rm = model.SpineParams.spineRM/(complen*circumf)
-    cm = model.SpineParams.spineCM*compdia*circumf
-    if cm<1e-15:
-        cm=1e-15
+              CM*complen*circumf)
+    comp.Ra = RA*complen/XArea
+    comp.Rm = RM/(complen*circumf)
+    cm = CM*compdia*circumf
+    if cm < 1e-15:
+        cm = 1e-15
     comp.Cm = cm
     comp.Em = model.SpineParams.spineELEAK
     comp.initVm = model.SpineParams.spineEREST
 
-def makeSpine(model, parentComp, compName,index, frac, necklen, neckdia, headdia):
+def makeSpine(model, parentComp, compName,index,frac,SpineParams):
     #frac is where along the compartment the spine is attached
     #unfortunately, these values specified in the .p file are not accessible
     neck_path = '{}/{}{}{}'.format(parentComp.path, compName, index, NAME_NECK)
@@ -39,39 +42,54 @@ def makeSpine(model, parentComp, compName,index, frac, necklen, neckdia, headdia
     z=parentComp.z0+ frac * (parentComp.z - parentComp.z0)
     neck.x0, neck.y0, neck.z0 = x, y, z
     #could pass in an angle and use cos and sin to set y and z
-    neck.x, neck.y, neck.z = x, y + necklen, z
-    setSpineCompParams(model, neck,neckdia,necklen)
+    neck.x, neck.y, neck.z = x, y + SpineParams.necklen, z
+    setSpineCompParams(model, neck,SpineParams.neckdia,SpineParams.necklen,SpineParams.neckRA,SpineParams.spineRM,SpineParams.spineCM)
 
     head_path = '{}/{}{}{}'.format(parentComp.path, compName, index, NAME_HEAD)
     head = moose.Compartment(head_path)
     moose.connect(neck, 'raxial', head, 'axial', 'Single')
     head.x0, head.y0, head.z0 = neck.x, neck.y, neck.z
-    head.x, head.y, head.z = head.x0, head.y0 + headdia, head.z0
+    head.x, head.y, head.z = head.x0, head.y0 + SpineParams.headlen, head.z0
 
-    setSpineCompParams(model, head,neckdia,necklen)
+    setSpineCompParams(model, head,SpineParams.headdia,SpineParams.headlen,SpineParams.headRA,SpineParams.spineRM,SpineParams.spineCM)
 
     return head
 
 def addSpines(model, container,ghkYN,name_soma):
     headarray=[]
     SpineParams = model.SpineParams
+    suma = 0
+
+    modelcond = model.Condset[container]
+    
     for comp in moose.wildcardFind(container + '/#[TYPE=Compartment]'):
         if name_soma not in comp.path:
-            numSpines=int(np.round(SpineParams.spineDensity*comp.length))
-            spineSpace=comp.length/(numSpines+1)
+            numSpines = int(np.round(SpineParams.spineDensity*comp.length))
+            if not numSpines:
+                 rand = random.random()
+                 if rand > SpineParams.spineDensity*comp.length:
+                     numSpines = 1
+                     suma += 1
+           
+            spineSpace = comp.length/(numSpines+1)
             for index in range(numSpines):
-                frac=(index+0.5)/numSpines
+                frac = (index+0.5)/numSpines
                 #print comp.path,"Spine:", index, "located:", frac
-                head=makeSpine(model, comp, 'sp',index, frac, SpineParams.necklen, SpineParams.neckdia, SpineParams.headdia)
+                head = makeSpine(model, comp, 'sp',index, frac, SpineParams)
                 headarray.append(head)
                 if SpineParams.spineChanList:
                     if ghkYN:
                         ghkproto=moose.element('/library/ghk')
                         ghk=moose.copy(ghkproto,comp,'ghk')[0]
                         moose.connect(ghk,'channel',comp,'channel')
-                    for chanpath,cond in zip(SpineParams.spineChanlist,SpineParams.spineCond):
+                    chan_list = []
+                    for c in SpineParams.spineChanList:
+                        chan_list.extend(c)
+                    for chanpath in chan_list:
+                        cond = distance_mapping(modelcond[chanpath],head)
                         addOneChan(chanpath,cond,head,ghkYN)
             #end for index
     #end for comp
+    
     log.info('{} spines created in {}', len(headarray), container)
     return headarray
