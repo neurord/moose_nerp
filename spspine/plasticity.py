a7adea21f20737ddc8d4f19679fdc892f1fdbb96
Asia Jedrzejewska-Szmek <jjedrzej@gmu.edu>
2017-03-27 15:22:24 -0400

    Account for DifShells and CaConc

--- spspine/plasticity.py
+++ spspine/plasticity.py
@@ -12,36 +12,58 @@ log = logutil.Logger()
 NAME_PLAS='/plas'
 NAME_CUM='Cum'
 
-def plasticity(synchan,name_calcium,Thigh,Tlow,highfac,lowfac):
-    compname = os.path.dirname(synchan.path)
-    calname = compname + '/'+name_calcium
-    cal=moose.element(calname)
-    shname=synchan.path+'/SH'
-    sh=moose.element(shname)
+def plasticity(synchan,synapse_params):
 
+    
+    comp = synchan.parent
+    for child in comp.children:
+        if child.className == 'CaConc' or child.className == 'ZombieCaConc':
+            cal = child
+            CaMSG = 'concOut'
+            break
+        elif child.className == 'DifShell':
+            cal = child
+            CaMSG = 'concentrationOut'
+            break
+    else:
+        print('Could not find calcium objects')
+        return
+    
+
+    shname = synchan.path+'/SH'
+    sh = moose.element(shname)
     log.debug("{} {} {}", synchan.path, sh.synapse[0], cal.path)
 
-    plasname=compname+'/'+NAME_PLAS
+    plasname=comp.path+'/'+NAME_PLAS
     plas=moose.Func(plasname)
+
     #FIRST: calculate the amount of plasticity
     #y is input plasticity trigger (e.g. Vm or Ca) 
-    moose.connect(cal,'concOut',plas,'yIn')
+    moose.connect(cal,CaMSG,plas,'yIn')
+    
     #x is the high threshold, z is the low threshold
     #This gives parabolic shape to plasticity between the low and high threshold
     #highfac and lowfac scale the weight change (set in SynParams.py)
-    expression=highfac+"(y>x)*(y-x)+(y>z)*(x>y)*(y-z)*(x-y)"+lowfac
-    plas.expr=expression
+    
+    highfac = synapse_params.highFactor
+    lowfac = synapse_params.lowFactor
+
+    expression = highfac+"(y>x)*(y-x)+(y>z)*(x>y)*(y-z)*(x-y)"+lowfac
+
+    plas.expr = expression
+    
     #Must define plasticity expression first, else these next assignments do nothing
-    plas.x=Thigh
-    plas.z=Tlow
+
+    plas.x = synapse_params.highThreshold
+    plas.z = synapse_params.lowThreshold
     #SECOND: accumulate all the changes, as percent increase or decrease
-    plasCum=moose.Func(plasname+NAME_CUM)
+    plasCum = moose.Func(plasname+NAME_CUM)
     #need input from the plasticity thresholding function to y 
     moose.connect(plas,'valueOut',plasCum,'xIn')
     moose.connect(plasCum,'valueOut',plasCum, 'yIn')
-    plasCum.expr="(x+1.0)*y*z"
-    plasCum.z=sh.synapse[0].weight
-    plasCum.y=1.0
+    plasCum.expr = "(x+1.0)*y*z"
+    plasCum.z = sh.synapse[0].weight
+    plasCum.y = 1.0
     moose.connect(plasCum,'valueOut',sh.synapse[0],'setWeight')
     
     return {'cum':plasCum,'plas':plas, 'syn': synchan}
@@ -49,17 +71,16 @@ def plasticity(synchan,name_calcium,Thigh,Tlow,highfac,lowfac):
 def addPlasticity(cell_pop,caplas_params):
     log.info("{} ", cell_pop)
     plascum={}
+    
     for cell in cell_pop:
-        plascum[cell]={}
-        allsyncomp_list=moose.wildcardFind(cell+'/##/'+caplas_params.syntype+'[ISA=SynChan]')
+        
+        plascum[cell] = {}
+        allsyncomp_list = moose.wildcardFind(cell+'/##/'+caplas_params.Synapse.Name+'[ISA=SynChan]')
+
         for synchan in allsyncomp_list:
             #if synapse exists
             if moose.exists(synchan.path+'/SH'):
                 log.debug("{} {} {}", cell, synchan.path, moose.element(synchan.path+'/SH'))
-                synname=util.syn_name(synchan.path, spines.NAME_HEAD)
-                plascum[cell][synname]=plasticity(synchan, caplas_params.NAME_CALCIUM,
-                                                  caplas_params.highThresh,
-                                                  caplas_params.lowThresh,
-                                                  caplas_params.highfactor,
-                                                  caplas_params.lowfactor)
+                synname = util.syn_name(synchan.path, spines.NAME_HEAD)
+                plascum[cell][synname] = plasticity(synchan, caplas_params.Synapse)
     return plascum
