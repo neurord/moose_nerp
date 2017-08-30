a6094d38edd90398fc8393d14ae1b619619ecb2d
Asia Jedrzejewska-Szmek <jjedrzej@gmu.edu>
2017-08-30 05:19:56 -0400

    Fix obsolete string method

--- moose_nerp/graph/spine_graph.py
+++ moose_nerp/graph/spine_graph.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, division
 import numpy as np
-import string
 from matplotlib import pyplot as plt
 
 def spineFig(model,spinecatab,spinevmtab,simtime):
@@ -12,14 +11,14 @@ def spineFig(model,spinecatab,spinevmtab,simtime):
     for neurnum in range(len(model.neurontypes())):
         for oid in spinevmtab[neurnum]:
             #find()-2 removes Vm from name, split and join removes dend from name
-            name=string.join(oid.name[oid.name.find('_')-2:].split('dend'))
+            name = ''.join(oid.name[oid.name.find('_')-2:].split('dend'))
             plt.plot(t,oid.vector,label=name)
         plt.ylabel('Vm')
     if model.calYN:
         plt.subplot(212)
         for neurnum in range(len(model.neurontypes())):
             for oid in spinecatab[neurnum]:
-                name=string.join(oid.name[oid.name.find('_')-2:].split('dend'))
+                name=''.join(oid.name[oid.name.find('_')-2:].split('dend'))
                 plt.plot(t,1000*oid.vector,label=name)
             plt.ylabel('calcium, uM')
     plt.legend()
