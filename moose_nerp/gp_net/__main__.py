6b01f0c7100b9543107743eb5694a5d471c98be2
Mariya Prokhorenko <mariyap@schlemiel.krasnow.gmu.edu>
2017-10-25 13:16:37 -0400

    GP:new package for creating gp network

---
+++ moose_nerp/gp_net/__main__.py
@@ -0,0 +1,115 @@
+# -*- coding:utf-8 -*-
+
+######## GPnetSim.py ############
+"""\
+Create a network of GP neurons using dictionaries for channels, synapses, and network parameters
+
+Can use ghk for calcium permeable channels if ghkYesNo=1
+Optional calcium concentration in compartments (calcium=1)
+Optional synaptic plasticity based on calcium (plasyesno=1)
+Spines are optional (spineYesNo=1), but not allowed for network
+The graphs won't work for multiple spines per compartment
+"""
+from __future__ import print_function, division
+import logging
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+from pprint import pprint
+import moose 
+
+from moose_nerp.prototypes import (cell_proto,
+                     clocks,
+                     inject_func,
+                     create_network,
+                     tables,
+                     net_output,
+                     logutil,
+                     util,
+                     standard_options)
+from moose_nerp import (gp,gp_net)
+from moose_nerp.graph import net_graph, neuron_graph, spine_graph
+
+option_parser = standard_options.standard_options(default_injection_current=[50e-12, 100e-12])
+param_sim = option_parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+# -*- coding:utf-8 -*-
+log = logutil.Logger()
+
+#################################-----------create the model
+#overrides:
+gp.synYN = True
+gp.plasYN = False
+gp_net.single=False
+
+##create neuron prototypes with synapses and calcium
+MSNsyn,neuron = cell_proto.neuronclasses(gp)
+
+all_neur_types=neuron
+#FSIsyn,neuron = cell_proto.neuronclasses(FSI)
+#all_neur_types.update(neuron)
+
+#create network and plasticity
+population,connections,plas=create_network.create_network(gp, gp_net, all_neur_types)
+
+###------------------Current Injection
+if gp_net.num_inject<np.inf and not gp_net.single :
+    inject_pop=inject_func.inject_pop(population['pop'],gp_net.num_inject)
+else:
+    inject_pop=population['pop']
+pg=inject_func.setupinj(gp, param_sim.injection_delay,param_sim.injection_width,inject_pop)
+moose.showmsg(pg)
+##############--------------output elements
+if gp_net.single:
+    tables.graphtables(gp, all_neur_types,
+                       param_sim.plot_current,
+                       param_sim.plot_current_message,
+                       [])
+    if gp.synYN:
+        #overwrite plastab above, since it is empty
+        syntab, plastab=tables.syn_plastabs(connections,plas)
+    if gp.spineYN:
+        spinecatab,spinevmtab=tables.spinetabs(gp,neuron)
+else:
+    spiketab, vmtab, plastab, catab = net_output.SpikeTables(gp, population['pop'], gp_net.plot_netvm, plas, gp_net.plots_per_neur)
+
+########## clocks are critical
+## these function needs to be tailored for each simulation
+## if things are not working, you've probably messed up here.
+if gp_net.single:
+    simpath=['/'+neurotype for neurotype in all_neur_types]
+else:
+    #possibly need to setup an hsolver separately for each cell in the network
+    simpath=[gp_net.netname]
+clocks.assign_clocks(simpath, param_sim.simdt, param_sim.plotdt, param_sim.hsolve,gp.param_cond.NAME_SOMA)
+
+################### Actually run the simulation
+def run_simulation(injection_current, simtime):
+    print(u'◢◤◢◤◢◤◢◤ injection_current = {} ◢◤◢◤◢◤◢◤'.format(injection_current))
+    pg.firstLevel = injection_current
+    moose.reinit()
+    moose.start(simtime)
+
+traces, names = [], []
+for inj in param_sim.injection_current:
+    run_simulation(injection_current=inj, simtime=param_sim.simtime)
+    if gp_net.single:
+        for neurnum,neurtype in enumerate(gp.neurontypes()):
+            traces.append(vmtab[neurnum][0].vector)
+            names.append('{} @ {}'.format(neurtype, inj))
+        if gp.synYN:
+            net_graph.syn_graph(connections, syntab, param_sim.simtime)
+        if gp.spineYN:
+            spine_graph.spineFig(gp,spinecatab,spinevmtab, param_sim.simtime)
+    else: 
+        if gp_net.plot_netvm:
+            net_graph.graphs(population['pop'], param_sim.simtime, vmtab,catab,plastab)
+            net_output.writeOutput(gp, gp_net.outfile+str(inj),spiketab,vmtab,population)
+
+if gp_net.single:
+    neuron_graph.SingleGraphSet(traces, names, param_sim.simtime)
+    # block in non-interactive mode
+util.block_if_noninteractive()
---
+++ moose_nerp/gp_net/param_net.py
@@ -0,0 +1,84 @@
+#param_net.py
+####################### Populations
+from moose_nerp.prototypes.util import NamedList
+from moose_nerp.prototypes.ttables import TableSet
+from moose_nerp.prototypes import util as _util
+
+neur_distr=NamedList('neur_distr', 'neuronname spacing percent')
+
+netname='/gp'
+confile='gp_connect'
+outfile='gp_out'
+
+spacing=54e-6 #Fig. 2 Hernandez Parvabinum+ Neurons and Npas1+ Neurons 2015
+#pv+: 54e-6 n=41, npas1+: 60e-6 n=33, calculated by measuring distance between neuron pairs and calculating mean 
+#0,1,2 refer to x, y and z
+grid={}
+grid[0]={'xyzmin':0,'xyzmax':100e-6,'inc':spacing}
+grid[1]={'xyzmin':0,'xyzmax':100e-6,'inc':spacing}
+grid[2]={'xyzmin':0,'xyzmax':0,'inc':0}
+
+#Do not include a neuron type in pop_dict if the proto not created
+neuron1pop=neur_distr(neuronname='proto', spacing=grid,percent=0.54) #Table 2 Hernandez Parvabinum+ Neurons and Npas1+ Neurons 2015
+#calculated from percent composition of PV+=proto, Npas1+/FoxP2+=arky
+neuron2pop=neur_distr(neuronname='arky', spacing=grid,percent=0.46)
+pop_dict={'proto':neuron1pop,'arky': neuron2pop}
+
+chanvarSPN = {
+    'Krp': 0.04,
+    'KaF': 0.04,
+    'KaS': 0.04,
+    'Kir': 0.04,
+    'CaL13': 0.04,
+    'CaL12': 0.04,
+    'CaR': 0.04,
+    'CaN': 0.04,
+    'CaT': 0.04,
+    'NaF': 0.0,
+    'BKCa': 0.04,
+    'SKCa': 0.04,
+}
+chanvar={'proto':chanvarSPN, 'arky':chanvarSPN}
+
+####################### Connections
+connect=NamedList('connect','synapse pre post space_const=None probability=None')
+ext_connect=NamedList('ext_connect','synapse pre post postsyn_fraction')
+# add post_location to both of these - optionally specify e.g. prox vs distal for synapses
+
+tt_Ctx_SPN = TableSet('CtxSPN', 'Ctx_4x4',syn_per_tt=2)
+tt_Thal_SPN = TableSet('ThalSPN', 'Thal_4x4',syn_per_tt=2)
+
+MSNconnSpaceConst=125e-6
+##FSIconnSpaceConst=200e-6
+neuron1pre_neuron1post=connect(synapse='gaba', pre='proto', post='proto', space_const=MSNconnSpaceConst)
+neuron1pre_neuron2post=connect(synapse='gaba', pre='proto', post='arky', space_const=MSNconnSpaceConst)
+neuron2pre_neuron1post=connect(synapse='gaba', pre='arky', post='proto', space_const=MSNconnSpaceConst)
+neuron2pre_neuron2post=connect(synapse='gaba', pre='arky', post='arky', space_const=MSNconnSpaceConst)
+##FSIpre_D1post=connect(synapse='gaba', pre='FSI', post='D1', space_const=FSIconnSpaceConst)
+##FSIpre_D2post=connect(synapse='gaba', pre='FSI', post='D2', space_const=FSIconnSpaceConst)
+##FSIpre_FSIpost=connect(synapse='gaba', pre='FSI', post='FSI', space_const=FSIconnSpaceConst)
+ctx_neuron1post=ext_connect(synapse='ampa',pre=tt_Ctx_SPN,post='proto', postsyn_fraction=0.5)
+thal_neuron1post=ext_connect(synapse='ampa',pre=tt_Thal_SPN,post='proto', postsyn_fraction=0.5)
+ctx_neuron2post=ext_connect(synapse='ampa',pre=tt_Ctx_SPN,post='arky', postsyn_fraction=0.5)
+thal_neuron2post=ext_connect(synapse='ampa',pre=tt_Thal_SPN,post='arky', postsyn_fraction=0.5)
+#glu_FSI=connect(synapse='ampa',pre='timetable',post='FSI',)
+
+#one dictionary for each post-synaptic neuron class
+proto={}
+arky={}
+##FSI={}
+connect_dict={}
+##Collect the above connections into dictionaries organized by post-syn neuron, and synapse type
+proto['gaba']={'proto': neuron1pre_neuron1post, 'arky': neuron2pre_neuron1post}#, 'FSI': FSIpre_D1post}
+proto['ampa']={'extern1': ctx_neuron1post, 'extern2': thal_neuron1post}
+connect_dict['proto']=proto
+arky['gaba']={'arky': neuron1pre_neuron2post, 'arky': neuron2pre_neuron2post}#, 'FSI': FSIpre_D2post}
+arky['ampa']={'extern1': ctx_neuron2post, 'extern2': thal_neuron2post}
+connect_dict['arky']=arky
+#FSI['gaba']={'FSI': FSIpre_FSIpost}
+#FSI['ampa']={'extern': glu_FSI}
+#connect_dict['FSI']=FSI
+
+# m/sec - GABA and the Basal Ganglia by Tepper et al
+cond_vel=0.8
+mindelay=1e-3
