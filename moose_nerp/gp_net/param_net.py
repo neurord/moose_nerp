5298ca95d220b04fb3022e935d0ea7a542081c7d
Avrama Blackwell <avrama@gmu.edu>
2018-10-18 12:59:05 -0400

    gp: channel variances approximated from stdev of optimization

--- moose_nerp/gp_net/param_net.py
+++ moose_nerp/gp_net/param_net.py
@@ -14,8 +14,8 @@ spacing=54e-6 #Fig. 2 Hernandez Parvabinum+ Neurons and Npas1+ Neurons 2015
 #pv+: 54e-6 n=41, npas1+: 60e-6 n=33, calculated by measuring distance between neuron pairs and calculating mean 
 #0,1,2 refer to x, y and z
 grid={}
-grid[0]={'xyzmin':0,'xyzmax':400e-6,'inc':spacing}
-grid[1]={'xyzmin':0,'xyzmax':400e-6,'inc':spacing}
+grid[0]={'xyzmin':0,'xyzmax':100e-6,'inc':spacing}
+grid[1]={'xyzmin':0,'xyzmax':100e-6,'inc':spacing}
 grid[2]={'xyzmin':0,'xyzmax':0,'inc':0}
 
 #Do not include a neuron type in pop_dict if the proto not created
@@ -24,21 +24,37 @@ neuron1pop=neur_distr(neuronname='proto', spacing=grid,percent=0.54) #Table 2 He
 neuron2pop=neur_distr(neuronname='arky', spacing=grid,percent=0.46)
 pop_dict={'proto':neuron1pop,'arky': neuron2pop}
 
-chanvarSPN = {
-    'Krp': 0.04,
-    'KaF': 0.04,
-    'KaS': 0.04,
-    'Kir': 0.04,
-    'CaL13': 0.04,
-    'CaL12': 0.04,
-    'CaR': 0.04,
-    'CaN': 0.04,
-    'CaT': 0.04,
-    'NaF': 0.0,
-    'BKCa': 0.04,
-    'SKCa': 0.04,
+#from arky140F - loc _0, unless CV close to 1
+chanSTD_arky = {
+    'KDr': 0.0397,
+    'Kv3': 0.0386,
+    'KvS': 0.0743,
+    'KvF': 0.0173,
+    'KCNQ': 0.0267,
+    'BKCa': 0.0238,
+    'SKCa': 0.295,
+    'HCN1': 0.2454,
+    'HCN2': 0.253,
+    'Ca': 0.1671,
+    'NaF': 0.0635,
+    'NaS': 0.215,
 }
-chanvar={'proto':chanvarSPN, 'arky':chanvarSPN}
+#from proto154 - loc _0, unless CV close to 1
+chanSTD_proto = {
+    'KDr': 0.0487,
+    'Kv3': 0.0177,
+    'KvS': 0.0306,
+    'KvF': 0.0114,
+    'HCN1': 0.139,
+    'HCN2': 0.175,
+    'KCNQ': 0.068,
+    'Ca': .0384,
+    'NaF': 0.0302,
+    'NaS': 0.1308,
+    'BKCa': 0.0496,
+    'SKCa': 0.2048,
+}
+chanvar={'proto':chanSTD_arky, 'arky':chanSTD_proto}
 
 ####################### Connections
 connect=NamedList('connect','synapse pre post space_const=None probability=None')
@@ -49,28 +65,28 @@ ext_connect=NamedList('ext_connect','synapse pre post postsyn_fraction')
 tt_STN = TableSet('tt_STN', 'Ctx_4x4',syn_per_tt=2)
 tt_Str_SPN = TableSet('tt_Str', 'Thal_4x4',syn_per_tt=2)
 
-MSNconnSpaceConst=125e-6
-##FSIconnSpaceConst=200e-6
-neuron1pre_neuron1post=connect(synapse='gaba', pre='proto', post='proto', space_const=MSNconnSpaceConst)#internal post syn fraction in 10% Shink Smith 1995
-neuron1pre_neuron2post=connect(synapse='gaba', pre='proto', post='arky', space_const=MSNconnSpaceConst)
-neuron2pre_neuron1post=connect(synapse='gaba', pre='arky', post='proto', space_const=MSNconnSpaceConst)
-neuron2pre_neuron2post=connect(synapse='gaba', pre='arky', post='arky', space_const=MSNconnSpaceConst)
+ConnSpaceConst=125e-6
+
+neur1pre_neur1post=connect(synapse='gaba', pre='proto', post='proto', space_const=ConnSpaceConst)#internal post syn fraction in 10% Shink Smith 1995
+neur1pre_neur2post=connect(synapse='gaba', pre='proto', post='arky', space_const=ConnSpaceConst)
+neur2pre_neur1post=connect(synapse='gaba', pre='arky', post='proto', space_const=ConnSpaceConst)
+neur2pre_neur2post=connect(synapse='gaba', pre='arky', post='arky', space_const=ConnSpaceConst)
 #post syn fraction: what fraction of synapse is contacted by time tables specified in pre 
-ext2_neuron1post=ext_connect(synapse='ampa',pre=tt_STN,post='proto', postsyn_fraction=1.0)#change ctx to Str MSN, Corbit Whalen 2016 Table 2 connectivity parameters: Chumhma 2011, Shink Smith 1995, Miguelez 2012 
-ext1_neuron1post=ext_connect(synapse='gaba',pre=tt_Str_SPN,post='proto', postsyn_fraction=0.33)#ext1 = Str
-ext2_neuron2post=ext_connect(synapse='ampa',pre=tt_STN,post='arky', postsyn_fraction=1.0)#ext2 STN
-ext1_neuron2post=ext_connect(synapse='gaba',pre=tt_Str_SPN,post='arky', postsyn_fraction=0.33)
+ext2_neur1post=ext_connect(synapse='ampa',pre=tt_STN,post='proto', postsyn_fraction=1.0)#change ctx to Str MSN, Corbit Whalen 2016 Table 2 connectivity parameters: Chumhma 2011, Shink Smith 1995, Miguelez 2012 
+ext1_neur1post=ext_connect(synapse='gaba',pre=tt_Str_SPN,post='proto', postsyn_fraction=0.33)#ext1 = Str
+ext2_neur2post=ext_connect(synapse='ampa',pre=tt_STN,post='arky', postsyn_fraction=1.0)#ext2 STN
+ext1_neur2post=ext_connect(synapse='gaba',pre=tt_Str_SPN,post='arky', postsyn_fraction=0.33)
 #one dictionary for each post-synaptic neuron class
 proto={}
 arky={}
 connect_dict={}
 ##Collect the above connections into dictionaries organized by post-syn neuron, and synapse type
-#the dictionary key fot tt must have 'extern' in it
-proto['gaba']={'proto': neuron1pre_neuron1post, 'arky': neuron2pre_neuron1post, 'extern': ext1_neuron1post}
-proto['ampa']={'extern': ext2_neuron1post}
+#the dictionary key for tt must have 'extern' in it
+proto['gaba']={'proto': neur1pre_neur1post, 'arky': neur2pre_neur1post, 'extern': ext1_neur1post}
+proto['ampa']={'extern': ext2_neur1post}
 connect_dict['proto']=proto
-arky['gaba']={'proto': neuron1pre_neuron2post, 'arky': neuron2pre_neuron2post, 'extern': ext1_neuron2post}
-arky['ampa']={'extern': ext2_neuron2post}
+arky['gaba']={'proto': neur1pre_neur2post, 'arky': neur2pre_neur2post, 'extern': ext1_neur2post}
+arky['ampa']={'extern': ext2_neur2post}
 connect_dict['arky']=arky
 
 # m/sec - GABA and the Basal Ganglia by Tepper et al
