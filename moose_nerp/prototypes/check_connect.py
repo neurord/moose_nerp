4285f77fc06986130da22fe96f75ebab9b701dcd
Avrama Blackwell <avrama@gmu.edu>
2019-01-10 17:27:22 -0500

    check_connect: update call to create_synpath_array using connection probability update calculation of needed time tables based on connection probability

--- moose_nerp/prototypes/check_connect.py
+++ moose_nerp/prototypes/check_connect.py
@@ -22,7 +22,7 @@ dist_incr = 1.0
 min_dist=0.01
 max_dist=5.0
 
-def count_postsyn(netparams,synapse_density,population):
+def count_postsyn(netparams,NumSyn,population):
     num_postcells={}  #dictionary of number of cells by type
     num_postsyn={}   #dictionary of post-synaptic receptors by cell type and synaptic receptor
     for ntype in netparams.connect_dict.keys():   #top level key is post-synaptic type
@@ -36,52 +36,56 @@ def count_postsyn(netparams,synapse_density,population):
         #allsyncomp_list = moose.wildcardFind(neur_proto + '/##[ISA=SynChan]')
         for syntype in netparams.connect_dict[ntype].keys():  #next level is synaptic receptor
             allsyncomp_list = moose.wildcardFind(neur_proto + '/##/'+syntype+'[ISA=SynChan]')
-            syncomps,totalsyn=connect.create_synpath_array(allsyncomp_list,syntype,synapse_density)
+            syncomps,totalsyn=connect.create_synpath_array(allsyncomp_list,syntype,NumSyn)
             num_postsyn[ntype][syntype]=num_postcells[ntype]*totalsyn
-    return num_postsyn,num_postcells
+    return num_postsyn,num_postcells,allsyncomp_list
 
 def count_presyn(netparams,num_cells,volume):
     presyn_cells={}
-    for postype in netparams.connect_dict.keys():
-        for syntype in netparams.connect_dict[postype].keys():
-            presyn_cells[postype]={}
-            presyn_cells[postype][syntype]=0
-            for presyn_type in netparams.connect_dict[postype][syntype].keys():
-                if 'extern' not in presyn_type:
-                    predict_cells=0
-                    space_const=netparams.connect_dict[postype][syntype][presyn_type].space_const
-                    density=num_cells[presyn_type]/volume
-                    max_cells=num_cells[presyn_type]
-                    inner_area=0
-                    for dist in np.arange(min_dist*space_const,max_dist*space_const,dist_incr*space_const):
-                        outer_area=np.pi*dist*dist
-                        predict_cells+=np.int(density*(outer_area-inner_area)*np.exp(-dist/space_const))
-                        log.debug("dist {} outer_area {} predict_cells {} ",  dist, outer_area, predict_cells)
-                        inner_area=outer_area
-                    presyn_cells[postype][syntype]+=min(max_cells,predict_cells)
-                    log.debug ("vol {} max_cells {} num_presyn {}",volume, max_cells, presyn_cells[postype][syntype])
+    for ntype,neur_connects in netparams.connect_dict.items():
+        presyn_cells[ntype]={}
+        for syntype,syn_connects in neur_connects.items():
+            presyn_cells[ntype][syntype]=0
+            intern_keys=[key for key in syn_connects.keys() if 'extern' not in key]
+            for presyn_type in intern_keys:
+                predict_cells=0
+                space_const=netparams.connect_dict[ntype][syntype][presyn_type].space_const
+                density=num_cells[presyn_type]/volume
+                max_cells=num_cells[presyn_type]
+                inner_area=0
+                for dist in np.arange(min_dist*space_const,max_dist*space_const,dist_incr*space_const):
+                    outer_area=np.pi*dist*dist
+                    predict_cells+=np.int(density*(outer_area-inner_area)*np.exp(-dist/space_const))
+                    log.debug("dist {} outer_area {} predict_cells {} ",  dist, outer_area, predict_cells)
+                    inner_area=outer_area
+                presyn_cells[ntype][syntype]+=min(max_cells,predict_cells)
+                log.debug ("vol {} max_cells {} num_presyn {}",volume, max_cells, presyn_cells[ntype][syntype])
     return presyn_cells
 
-def count_total_tt(netparams,num_postsyn,num_postcells):
+def count_total_tt(netparams,num_postsyn,num_postcells,allsyncomp_list,NumSyn):
     tt_needed_per_syntype={}
     tt_per_ttfile={}
     for each in ttables.TableSet.ALL:
         tt_per_ttfile[each.tablename]={}
         each.needed=0
-    #Determine how many trains of synaptic input are needed needed.
-    for ntype in netparams.connect_dict.keys():
+    #Determine how many trains of synaptic input are needed.
+    for ntype,neur_connects in netparams.connect_dict.items():
         tt_needed_per_syntype[ntype]={}
         if num_postcells[ntype]:
-            for syntype in netparams.connect_dict[ntype].keys():
+            for syntype,syn_connects in neur_connects.items():
                 needed_trains=0
-                for key in netparams.connect_dict[ntype][syntype].keys():
-                  if 'extern' in key:
-                      ttname=netparams.connect_dict[ntype][syntype][key].pre
-                      dups=netparams.connect_dict[ntype][syntype][key].pre.syn_per_tt
-                      postsyn_fraction=netparams.connect_dict[ntype][syntype][key].postsyn_fraction
-                      needed_trains+=np.int(np.ceil(num_postsyn[ntype][syntype]*postsyn_fraction/dups))
-                      tt_per_ttfile[ttname.tablename][ntype]={'num': np.int(np.ceil(num_postsyn[ntype][syntype]*postsyn_fraction/dups)), 'syn_per_tt': dups}
-                      log.debug('tt {} syn_per_tt {} postsyn_fraction {} needed_trains {}',key, dups,postsyn_fraction,needed_trains)
+                extern_keys=[key for key in syn_connects.keys() if 'extern' in key]
+                for pretype in extern_keys:
+                    ttname=syn_connects[pretype].pre
+                    dups=syn_connects[pretype].pre.syn_per_tt
+                    if syn_connects[pretype].dend_loc:
+                        dend_prob=syn_connects[pretype].dend_loc
+                    else:
+                        dend_prob=None
+                    syncomps,totalsyn=connect.create_synpath_array(allsyncomp_list,syntype,NumSyn,prob=dend_prob)
+                    needed_trains+=np.int(np.ceil(totalsyn/dups))
+                    tt_per_ttfile[ttname.tablename][ntype]={'num': np.int(np.ceil(totalsyn/dups)), 'syn_per_tt': dups}
+                    log.debug('tt {} syn_per_tt {} postsyn_prob {} needed_trains {}',pretype, dups,dend_prob,needed_trains)
                 tt_needed_per_syntype[ntype][syntype]=needed_trains
     for each in ttables.TableSet.ALL:
         for ntype in tt_per_ttfile[each.tablename].keys():
@@ -98,11 +102,11 @@ def check_netparams(netparams,NumSyn,population=[]):
         population={}
         for ntype in netparams.connect_dict.keys():
             population[ntype]=np.arange(np.round(num_neurons*netparams.pop_dict[ntype].percent))
-    log.debug("pop {}",population)
-    num_postsyn,num_postcells=count_postsyn(netparams,NumSyn,population)
+    log.info("pop {}",population)
+    num_postsyn,num_postcells,allsyncomp_list=count_postsyn(netparams,NumSyn,population)
     log.info("num synapses {} cells {}", num_postsyn, num_postcells)
-    tt_per_syn,tt_per_ttfile=count_total_tt(netparams,num_postsyn,num_postcells)
-    log.debug("num time tables needed: per synapse {} per ttfile {}", tt_per_syn, tt_per_ttfile)
+    tt_per_syn,tt_per_ttfile=count_total_tt(netparams,num_postsyn,num_postcells,allsyncomp_list,NumSyn)
+    log.info("num time tables needed: per synapse {} per ttfile {}", tt_per_syn, tt_per_ttfile)
     presyn_cells=count_presyn(netparams,num_postcells,volume)
     log.info("num presyn_cells {}", presyn_cells)
     for ntype in netparams.connect_dict.keys():
