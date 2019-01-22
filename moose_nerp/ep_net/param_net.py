7ee18dbe39af43e1f7bb391e65e4f48ea11856de
Avrama Blackwell <avrama@gmu.edu>
2019-01-22 17:12:20 -0500

    ep_net: parameters for branch distance dependent connections

--- moose_nerp/ep_net/param_net.py
+++ moose_nerp/ep_net/param_net.py
@@ -6,7 +6,7 @@ from moose_nerp.prototypes import util as _util
 
 neur_distr=NamedList('neur_distr', 'neuronname spacing percent')
 
-netname='/ep'
+netname='/epnet'
 confile='ep_connect'
 outfile='ep_out'
 
@@ -42,19 +42,29 @@ chanvar={'ep':chanSTD}
 
 ####################### Connections
 #for improved NetPyne correspondance: change synapse to synMech, change pre to source
-#mindist, maxdist, half_dist, steep are alternatives to postsyn_fraction (ext_connect) or probability (connect)
-#these refer to dendritic location of post-synaptic target as follows:
+#Two types of probabilities controlling the connections
+#A. probability of connecting two different neurons.  NamedList('connect'Parameters include
+#A1. constant probability
+#A2. space_const: allows distance dependent connection, where distance is measured between pre- and post-synaptic neuron's cell bodies
+#A3. num_conns allows a single pre-synaptic cell to make more than one connection on the post-synaptic cell
+#B. dend_loc, which controls the dendritic location of post-synaptic target as follows
+#mindist, maxdist, half_dist, steep  are alternatives to postsyn_fraction 
 #connect_prob=0 if dist<mindist
 #connect_prob=0 if dist>maxdist
-#connect_prob = probability if dist between mindist and maxdist
+#connect_prob = probability if dist between mindist and maxdist, or
 #if half_dist is defined:
 #for steep>0: connect_prob=1 if dist>maxdist and 0 if dist<mindist 
 #connect_prob=(dist-mindist)^steep/((dist-mindist)^steep+half_dist^steep)
 #make steep<0 to switch slope and have connect_prob=1 if dist<mindist and 0 if dist>maxdist
 #do not use steep (or set to zero) to have constant connection probability between min and maxdist
+
+#Intrinsic (within network) connections specified using NamedList('connect'
+#Extrinsic (external time table) connections specified using NamedList('ext_connect'
+
 dend_location=NamedList('dend_location','mindist=0 maxdist=1 maxprob=None half_dist=None steep=0 postsyn_fraction=None')
-#probability for intrinsic is the probability of connecting pre and post. space_const allows cell body distance dependence
-connect=NamedList('connect','synapse pre post space_const=None probability=None dend_loc=None')
+
+#probability for intrinsic is the probability of connecting pre and post.
+connect=NamedList('connect','synapse pre post num_conns=2 space_const=None probability=None dend_loc=None')
 ext_connect=NamedList('ext_connect','synapse pre post dend_loc=None')
 
 #tables of extrinsic inputs
@@ -66,23 +76,23 @@ tt_GPe = TableSet('tt_GPe', 'GPe_4x4',syn_per_tt=2)
 ConnSpaceConst=125e-6
 
 #description of intrinsic inputs
-#neur1pre_neur1post=connect(synapse='gaba', pre='ep', post='gaba', probability=0)#need reference for no internal connections
+ep_distr=dend_location(mindist=30e-6,maxdist=100e-6,postsyn_fraction=1,half_dist=50e-6,steep=1)
+neur1pre_neur1post=connect(synapse='gaba', pre='ep', post='gaba', probability=0.5,dend_loc=ep_distr)#need reference for no internal connections
 #description of synapse and dendritic location of extrinsic inputs
 GPe_distr=dend_location(mindist=0,maxdist=60e-6,half_dist=30e-6,steep=-1)
 Str_distr=dend_location(mindist=30e-6,maxdist=1000e-6,postsyn_fraction=1,half_dist=100e-6,steep=1)
-STN_distr=dend_location(postsyn_fraction=0.5)
-#post syn fraction: what fraction of synapse is contacted by time tables specified in pre 
+STN_distr=dend_location(postsyn_fraction=0.25)
+
 ext1_neur1post=ext_connect(synapse='ampa',pre=tt_STN,post='ep', dend_loc=STN_distr)# need reference
 ext2_neur1post=ext_connect(synapse='gaba',pre=tt_GPe,post='ep', dend_loc=GPe_distr)
 ext3_neur1post=ext_connect(synapse='gaba',pre=tt_STR,post='ep', dend_loc=Str_distr)
-#ext1_neur1post=ext_connect(synapse='ampa',pre=tt_STN,post='ep')# need reference
 
 #Collect all connection information into dictionaries
 #1st create one dictionary for each post-synaptic neuron class
 ep={}
 #connections further organized by synapse type
 #the dictionary key for tt must have 'extern' in it
-ep['gaba']={'extern2': ext2_neur1post, 'extern3':ext3_neur1post}
+ep['gaba']={'extern2': ext2_neur1post, 'extern3': ext3_neur1post, 'ep':neur1pre_neur1post}
 ep['ampa']={'extern1': ext1_neur1post}
 
 #Then, collect the post-synaptic dictionaries into a single dictionary.
