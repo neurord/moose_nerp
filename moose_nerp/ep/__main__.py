9150e39a86b57712adcddea360d5afde231b99b6
Avrama Blackwell <avrama@gmu.edu>
2019-03-28 17:34:00 -0400

    ep: add code for simulating stp vs input freq

--- moose_nerp/ep/__main__.py
+++ moose_nerp/ep/__main__.py
@@ -23,10 +23,35 @@ running a simulation, the output tables would be accessible as model.vmtab,
 model.catab, etc.'''
 
 # Parameter overrides can be specified:
+model.synYN = True
+model.stpYN = False
 
 # This function sets up the options specified in param_sim or passed from
 # command line:
 create_model_sim.setupOptions(model)
+param_sim = model.param_sim
+param_sim.injection_delay = 0.0
+
+############ Vary the next three parameters to evaluate response to regular synaptic stimulation
+param_sim.injection_current = [-80e-12] #choose from [0,-80e-12]
+syntype='str' #choose from 'str', 'GPe'
+stimfreq=10 #choose from 1,5,10,20,40
+
+syn_delay=1.0
+minpulses=10
+import moose
+tt=moose.TimeTable('tt'+str(stimfreq)+'hz')
+if stimfreq<minpulses:
+    param_sim.simtime = syn_delay+minpulses/float(stimfreq)
+    tt.vector=[syn_delay+n/float(stimfreq) for n in range(minpulses)]
+else:
+    param_sim.simtime = syn_delay+1.1
+    tt.vector=[syn_delay+n/float(stimfreq) for n in range(stimfreq)]
+param_sim.injection_width = param_sim.simtime-param_sim.injection_delay
+
+param_sim.plot_synapse=True
+param_sim.plot_synapse=True
+param_sim.plot_calcium=False
 
 # This function creates the neuron(s) in Moose:
 create_model_sim.setupNeurons(model)
@@ -57,42 +82,34 @@ create_model_sim.setupStim(model)
 # the new options are explicitly called.
 
 #specify presyn as tt explicitly and test.
-import moose
-import numpy as np
 from moose_nerp.prototypes import plasticity
 from moose_nerp import ep_net as net
 
-simtime=model.param_sim.simtime
-#simtime=5
-tt1=moose.TimeTable('tt1hz')
-tt5=moose.TimeTable('tt5hz')
-tt10=moose.TimeTable('tt10hz')
-tt20=moose.TimeTable('tt20hz')
-tt40=moose.TimeTable('tt40hz')
-tt1.vector=[0.5+n/1.0 for n in range(int(simtime*1))]
-tt5.vector=[0.5+n/5.0 for n in range(int(simtime*5))]
-tt10.vector=[0.5+n/10.0 for n in range(10)]
-tt20.vector=[0.5+n/20.0 for n in range(20)]
-tt40.vector=[0.5+n/40.0 for n in range(40)]
+if syntype=='str':
+    synchan=moose.element('/ep[0]/p0b1b1b1[0]/gaba[0]/')#str synchan
+    stp_params=net.param_net.str_plas
+elif syntype=='GPe':
+    synchan=moose.element('/ep[0]/p0b1[0]/gaba[0]/') #gp synchan
+    stp_params=net.param_net.GPe_plas
+else:
+    print('unknown synapse type')
 
-tt=tt10
-synchan=moose.element('/ep[0]/p0b1[0]/gaba[0]/') #gp synchan
-#synchan=moose.element('/ep[0]/p0b1b1b1[0]/gaba[0]/')#str synchan
 sh=moose.element(synchan.path+'/SH')
 sh.numSynapses=1
 moose.connect(tt,'eventOut', sh.synapse[0], 'addSpike')
 
-stp_params=net.param_net.GPe_plas
-#stp_params=net.param_net.Str_plas
 simdt =  model.param_sim.simdt
 plasticity.ShortTermPlas(sh.synapse[0],0,stp_params,simdt,tt,'eventOut')
-#for ii in range(32):
-#    moose.setClock(ii, simdt)
 
 #evaluate result:
-deptab = moose.Table('/deptab')
-dep0=moose.element(synchan.path+'/dep0')
-moose.connect(deptab, 'requestOut', dep0, 'getValue')
+if stp_params.depress is not None:
+    deptab = moose.Table('/deptab')
+    dep=moose.element(synchan.path+'/dep0')
+    moose.connect(deptab, 'requestOut', dep, 'getValue')
+if stp_params.facil is not None:
+    factab = moose.Table('/factab')
+    fac=moose.element(synchan.path+'/fac0')
+    moose.connect(factab, 'requestOut', fac, 'getValue')
 plas_tab = moose.Table('/plastab')
 plas=moose.element(synchan.path+'/stp0')
 moose.connect(plas_tab, 'requestOut', plas, 'getValue')
@@ -104,14 +121,32 @@ create_model_sim.runAll(model,printParams=False)
 #moose.reinit()
 #moose.start(simtime)
 
+print('*********** freq',stimfreq,'simtime',param_sim.simtime,'tt',tt.vector)
 from matplotlib import pyplot as plt
+import numpy as np
 plt.ion()
 plt.figure()
+plt.title('synapse')
 numpts=len(plas_tab.vector)
 time=np.arange(0,simdt*numpts,simdt)
-plt.plot(time[0:numpts],deptab.vector,label='dep')
+if stp_params.depress is not None:
+    plt.plot(time[0:numpts],deptab.vector,label='dep')
+if stp_params.facil is not None:
+    plt.plot(time[0:numpts],factab.vector,label='fac')
 plt.plot(time[0:numpts],plas_tab.vector+0.1,label='plas+0.1')
 plt.plot(time[0:numpts],syn_tab.vector*1e9,label='Gk*1e9')
 plt.legend()
 
+#Extract amplitude of PSPs based on knowledge of spike time
+vmtab=model.vmtab['ep'][0].vector
+vm_init=[vmtab[int(t/simdt)] for t in tt.vector]
+#use np.min for IPSPs and np.max for EPSPs
+vm_peak=[np.min(vmtab[int(tt.vector[i]/simdt):int(tt.vector[i+1]/simdt)]) for i in range(len(tt.vector)-1)]
+psp_amp=[(vm_init[i]-vm_peak[i]) for i in range(len(vm_peak))]
+psp_norm=[amp/psp_amp[0] for amp in psp_amp]
+pulse=range(len(psp_norm))
+plt.figure()
+plt.title('Noramlized PSP amp for '+syntype+' at '+str(stimfreq)+'hz')
+plt.plot(pulse,psp_norm,'o')
 
+##Now, save vmtab, psp_amp, psp_norm so can plot the entire set 
