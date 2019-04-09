c88b5199b926991105df16bb8c3080dbdfe87d63
Avrama Blackwell <avrama@gmu.edu>
2019-04-09 15:46:11 -0400

    ep: freq dependence plasticity tests moved to other functions

--- moose_nerp/ep/__main__.py
+++ moose_nerp/ep/__main__.py
@@ -22,38 +22,36 @@ namespace as argument, and append variables to this namespace. Thus, after
 running a simulation, the output tables would be accessible as model.vmtab,
 model.catab, etc.'''
 
+model.synYN = True
+model.stpYN = True
+presyn='GPe' #choose from 'str', 'GPe'
+stimfreq=20 #choose from 1,5,10,20,40
+model.param_sim.stim_paradigm='PSP_'+str(stimfreq)+'Hz'
+
 # This function sets up the options specified in param_sim or passed from
 # command line:
 create_model_sim.setupOptions(model)
+
 # Parameter overrides can be specified:
 param_sim = model.param_sim
+param_sim.injection_current = [-1e-12] #offset to prevent or enhance firing
 param_sim.injection_delay = 0.0
-model.synYN = True
-model.stpYN = True
-param_sim.save_txt=True
+param_sim.save_txt=False
 param_sim.plot_synapse=True
 param_sim.plot_calcium=False
 
-############ Vary the next three parameters to evaluate response to regular synaptic stimulation
-param_sim.injection_current = [-1e-12] #choose from [50e012,-50e-12]
-syntype='GPe' #choose from 'str', 'GPe'
-stimfreq=10 #choose from 1,5,10,20,40
-
-syn_delay=1.0
-minpulses=10
-import moose
-tt=moose.TimeTable('tt'+str(stimfreq)+'hz')
-if stimfreq<minpulses:
-    param_sim.simtime = syn_delay+minpulses/float(stimfreq)
-    tt.vector=[syn_delay+n/float(stimfreq) for n in range(minpulses)]
+#
+from moose_nerp import ep_net as net
+if presyn=='str':
+    stp_params=net.param_net.str_plas
+elif presyn=='GPe':
+    stp_params=net.param_net.GPe_plas
 else:
-    param_sim.simtime = syn_delay+1.1
-    tt.vector=[syn_delay+n/float(stimfreq) for n in range(stimfreq)]
-param_sim.injection_width = param_sim.simtime-param_sim.injection_delay
+    print('########### unknown synapse type')
 
-param_sim.fname='ep_syn'+syntype+'_freq'+str(stimfreq)+'_plas'+str(1 if model.stpYN else 0)
-param_dict={'syn':syntype,'freq':stimfreq,'plas':model.stpYN,'inj':param_sim.injection_current}
-outfile=param_sim.fname+'_inj'+str(param_sim.injection_current[0])
+param_sim.fname='ep_syn'+presyn+'_freq'+str(stimfreq)+'_plas'+str(1 if model.stpYN else 0)+'_inj'+str(param_sim.injection_current[0])
+print('>>>>>>>>>> moose_main, stimfreq {} presyn {} stpYN {}'.format(stimfreq,presyn,model.stpYN))
+param_dict={'syn':presyn,'freq':stimfreq,'plas':model.stpYN,'inj':param_sim.injection_current}
 
 # This function creates the neuron(s) in Moose:
 create_model_sim.setupNeurons(model)
@@ -72,7 +70,6 @@ create_model_sim.setupStim(model)
 # This function runs all the specified simulations, plotting and saving them
 # as specified:
 #create_model_sim.runAll(model,printParams=False)
-
 # Alternative function to create_model_sim.runAll, that runs a simulation a few
 # steps at a time and then updates a plot, to show the live simulation results.
 # This is an example of modifying, expanding, or customizing code:
@@ -83,27 +80,19 @@ create_model_sim.setupStim(model)
 # with new options that do not alter the current state of the functions unless
 # the new options are explicitly called.
 
-#specify presyn as tt explicitly and test
-#This code should be moved to plasticity_test
-
-from moose_nerp.prototypes import plasticity_test as plas_test
-from moose_nerp import ep_net as net
-
-if syntype=='str':
-    synchan=moose.element('/ep[0]/p0b1b1b2[0]/gaba[0]/')#str synchan
-    stp_params=net.param_net.str_plas
-elif syntype=='GPe':
-    synchan=moose.element('/ep[0]/p0b1[0]/gaba[0]/') #gp synchan
-    stp_params=net.param_net.GPe_plas
-else:
-    print('########### unknown synapse type')
-
 #add short term plasticity to synapse as appropriate
-if model.stpYN:
-    syntab,plastabset=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0,
-                                                        simdt=model.param_sim.simdt,stp_params=stp_params)
-else:
-    syntab=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0)
+from moose_nerp.prototypes import plasticity_test as plas_test
+syntab={ntype:[] for ntype in  model.neurons.keys()}
+plastabset={ntype:[] for ntype in  model.neurons.keys()}
+for ntype in model.neurons.keys():
+    for tt_syn_pair in model.pairs[ntype].values():
+        tt=tt_syn_pair[0]
+        synchan=tt_syn_pair[1]
+        if model.stpYN:
+            syntab[ntype],plastabset[ntype]=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0,
+                                                                   simdt=model.param_sim.simdt,stp_params=stp_params)
+        else:
+            syntab[ntype]=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0)
 
 #simulate the model
 create_model_sim.runAll(model,printParams=False)
@@ -115,15 +104,15 @@ import numpy as np
 plt.ion()
 plt.figure()
 plt.title('synapse')
-numpts=len(syntab.vector)
-time=np.arange(0,syntab.dt*numpts,syntab.dt)
-if model.stpYN:
-    if 'dep' in plastabset.keys():
-        plt.plot(time[0:numpts],plastabset['dep'].vector,label='dep')
-    if 'fac' in plastabset.keys():
-        plt.plot(time[0:numpts],plastabset['fac'].vector,label='fac')
-    plt.plot(time[0:numpts],plastabset['plas'].vector+0.1,label='plas+0.1')
-plt.plot(time[0:numpts],syntab.vector*1e9,label='Gk*1e9')
+for ntype in model.neurons.keys():
+    numpts=len(syntab[ntype].vector)
+    time=np.arange(0,syntab[ntype].dt*numpts,syntab[ntype].dt)
+    if model.stpYN:
+        for i,tab in enumerate(plastabset[ntype]):
+            offset=i*0.02
+            labl=tab.name[-4:-1]+'+'+str(offset)
+            plt.plot(time[0:numpts],tab.vector+offset,label=labl)
+plt.plot(time[0:numpts],syntab[ntype].vector*1e9,label='Gk*1e9')
 plt.legend()
 
 #Extract spike times and calculate ISI if not hyperpolarized
@@ -133,7 +122,7 @@ spike_time={key:[] for key in vmtab.keys()}
 numspikes={key:[] for key in vmtab.keys()}
 isis={key:[] for key in vmtab.keys()}
 plt.figure()
-plt.title('ISI vs time for '+syntype+' at '+str(stimfreq)+'hz')
+plt.title('ISI vs time for '+presyn+' at '+str(stimfreq)+'hz')
 plt.xlabel('Time (msec)')
 plt.ylabel('ISI (sec)')
 for neurtype, tabset in vmtab.items():
@@ -142,7 +131,8 @@ for neurtype, tabset in vmtab.items():
         isis[neurtype].append(np.diff(spike_time[neurtype][-1]))
         plt.plot(spike_time[neurtype][-1][0:len(isis[neurtype][-1])],isis[neurtype][-1],'o')
     numspikes[neurtype]=[len(st) for st in spike_time[neurtype]]
-    print(neurtype,'mean:',np.mean(numspikes[neurtype]),'rate',np.mean(numspikes[neurtype])/param_sim.simtime,'from',numspikes[neurtype], 'spikes, ISI mean&STD: ',[np.mean(isi) for isi in isis[neurtype]], [np.std(isi) for isi in isis[neurtype]] )
+    print(neurtype,'mean:',np.mean(numspikes[neurtype]),'rate',np.mean(numspikes[neurtype])/param_sim.simtime,'from',numspikes[neurtype],
+          'spikes, ISI mean&STD: ',[np.mean(isi) for isi in isis[neurtype]], [np.std(isi) for isi in isis[neurtype]] )
 
 spike1=np.min(tt.vector)
 spikeN=np.max(tt.vector)
@@ -160,12 +150,13 @@ elif not len(stim_spikes):
     psp_norm=[amp/psp_amp[0] for amp in psp_amp]
     pulse=range(len(psp_norm))
     plt.figure()
-    plt.title('Normalized PSP amp for '+syntype+' at '+str(stimfreq)+'hz')
+    plt.title('Normalized PSP amp for '+presyn+' at '+str(stimfreq)+'hz')
     plt.plot(pulse,psp_norm,'o')
     plt.xlabel('stim number')
     ##save vmtab, psp_amp, psp_norm, spike_time, isi so can plot the entire set of frequencies
     if model.param_sim.save_txt:
         np.savez(outfile,amp=psp_amp,norm=psp_norm,params=param_dict)
+
 '''           tau decay = 20ms               Lavian       tau decay = 10 ms
               str      GPe,tau=1s  tau=0.6s  GPe    Str    str   GPe (tau=0.6s)
 initial PSP   0.4 mV   1.5 mV       1.5       3     1.5   
@@ -182,16 +173,14 @@ Traces in Lavian reveal that str decay gets _faster_ with higher frequencies,
 so decay between pulses does not decrease drastically
 Lower membrane time constant with higher tau decay may work better
 
-Perhaps get Dan's help in adding frequency parameters to command line changes
-
 #Analyze set of files
 import glob
 import numpy as np
 
-syntype='GPe'
+presyn='GPe'
 plasYN=1
 inj='-1e-10'
-pattern='ep_syn'+syntype+'*_plas'+str(plasYN)+'_inj'+inj+'*.npz'
+pattern='ep_syn'+presyn+'*_plas'+str(plasYN)+'_inj'+inj+'*.npz'
 files=glob.glob(pattern)
 results={}
 time={}
--- moose_nerp/ep/multisim.py
+++ moose_nerp/ep/multisim.py
@@ -1,39 +1,36 @@
 from __future__ import print_function, division
 def moose_main(p):
-    stimfreq,syntype,stpYN=p
+    stimfreq,presyn,stpYN=p
     import moose
     from moose_nerp.prototypes import create_model_sim
-    from moose_nerp.prototypes import plasticity
+    from moose_nerp.prototypes import plasticity_test as plas_test
     from moose_nerp import ep as model
     from moose_nerp import ep_net as net
 
-    syn_delay=1.0
-    minpulses=10
     model.synYN = True
     model.stpYN = stpYN
-
+    model.param_sim.stim_paradigm='PSP_'+str(stimfreq)+'Hz'
     create_model_sim.setupOptions(model)
     # Parameter overrides can be specified:
+    #stim_protocol='PSP_'+str(stimfreq)+'Hz'
+    #model.param_stim.Stimulation=model.param_stim.paradigm_dict[stim_protocol]
+
     param_sim = model.param_sim
-    param_sim.injection_delay = 0.0
+    param_sim.injection_current= [-25e-12] 
     param_sim.save_txt=False
-    param_sim.injection_current = [-1e-12] 
     param_sim.plot_synapse=True
     param_sim.plot_calcium=False
-    param_sim.fname='ep_syn'+syntype+'_freq'+str(stimfreq)+'_plas'+str(1 if model.stpYN else 0)
-    print('>>>>>>>>>> moose_main, stimfreq {} syntype {} stpYN {}'.format(stimfreq,syntype,stpYN))
 
-    tt=moose.TimeTable('tt'+str(stimfreq)+'hz')
-    if stimfreq<minpulses:
-        param_sim.simtime = syn_delay+minpulses/float(stimfreq)
-        tt.vector=[syn_delay+n/float(stimfreq) for n in range(minpulses)]
+    if presyn=='str':
+        stp_params=net.param_net.str_plas
+    elif presyn=='GPe':
+        stp_params=net.param_net.GPe_plas
     else:
-        param_sim.simtime = syn_delay+1.1
-        tt.vector=[syn_delay+n/float(stimfreq) for n in range(stimfreq)]
-    param_sim.injection_width = param_sim.simtime-param_sim.injection_delay
+        print('########### unknown synapse type')
 
-    param_dict={'syn':syntype,'freq':stimfreq,'plas':model.stpYN,'inj':param_sim.injection_current}
-    outfile=param_sim.fname+'_inj'+str(param_sim.injection_current[0])
+    param_sim.fname='ep_syn'+presyn+'_freq'+str(stimfreq)+'_plas'+str(1 if model.stpYN else 0)+'_inj'+str(param_sim.injection_current[0])
+    print('>>>>>>>>>> moose_main, stimfreq {} presyn {} stpYN {}'.format(stimfreq,presyn,stpYN))
+    param_dict={'syn':presyn,'freq':stimfreq,'plas':model.stpYN,'inj':param_sim.injection_current}
 
     # This function creates the neuron(s) in Moose:
     create_model_sim.setupNeurons(model)
@@ -45,57 +42,37 @@ def moose_main(p):
     # injection or synaptic stimulation:
     create_model_sim.setupStim(model)
 
-    if syntype=='str':
-        synchan=moose.element('/ep[0]/p0b1b1b2[0]/gaba[0]/')#str synchan
-        stp_params=net.param_net.str_plas
-    elif syntype=='GPe':
-        synchan=moose.element('/ep[0]/p0b1[0]/gaba[0]/') #gp synchan
-        stp_params=net.param_net.GPe_plas
-    else:
-        print('########### unknown synapse type')
-
-    #Connect time table of synaptic inputs to synapse
-    sh=moose.element(synchan.path+'/SH')
-    sh.numSynapses=1
-    moose.connect(tt,'eventOut', sh.synapse[0], 'addSpike')
-    #create output table for the synaptic response
-    syn_tab=moose.Table('/syntab')
-    moose.connect(syn_tab,'requestOut',synchan,'getGk')
-
-    #add short term plasticity to synapse as appropriate
-    if model.stpYN:
-        simdt =  model.param_sim.simdt
-        plasticity.ShortTermPlas(sh.synapse[0],0,stp_params,simdt,tt,'eventOut')
-        #Add output tables for plasticity
-        if stp_params.depress is not None:
-            deptab = moose.Table('/deptab')
-            dep=moose.element(synchan.path+'/dep0')
-            moose.connect(deptab, 'requestOut', dep, 'getValue')
-        if stp_params.facil is not None:
-            factab = moose.Table('/factab')
-            fac=moose.element(synchan.path+'/fac0')
-            moose.connect(factab, 'requestOut', fac, 'getValue')
-        plas_tab = moose.Table('/plastab')
-        plas=moose.element(synchan.path+'/stp0')
-        moose.connect(plas_tab, 'requestOut', plas, 'getValue')
+    #add short term plasticity to synapse as appropriate - need to modify this to NOT create or hook up tt, just create plas and output tables
+    syntab={ntype:[] for ntype in  model.neurons.keys()}
+    plastabset={ntype:[] for ntype in  model.neurons.keys()}
+    for ntype in model.neurons.keys():
+        for tt_syn_pair in model.pairs[ntype].values():
+            tt=tt_syn_pair[0]
+            synchan=tt_syn_pair[1]
+            if model.stpYN:
+                syntab[ntype],plastabset[ntype]=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0,
+                                                                                     simdt=model.param_sim.simdt,stp_params=stp_params)
+            else:
+                syntab[ntype]=plas_test.short_term_plasticity_test(synchan,tt,syn_delay=0)
 
     #simulate the model
     create_model_sim.runAll(model,printParams=False)
     print('<<<<<<<<<<< moose_main, sim {} finished'.format(param_sim.fname))
-    tab_dict={'syn':syn_tab.vector, 'tt': tt.vector,'dt':{'tabdt':syn_tab.dt,'ttdt':tt.dt}}
-    if model.stpYN:
-        tab_dict['plas']=plas_tab.vector
-        if stp_params.depress is not None:
-            tab_dict['dep']=deptab.vector
-        if stp_params.facil is not None:
-            tab_dict['fac']=factab.vector
+
+    #create dictionary with the output (vectors) from tables
+    tab_dict={}
+    for ntype,tabset in syntab.items():
+        tab_dict[ntype]={'syn':tabset.vector,'syndt':tabset.dt,
+        'tt': {ntype+'_'+pt:tab.vector for pt,tab in model.tt[ntype].items()}}#,'tt_dt':tabset.dt}
+        if model.stpYN:
+            tab_dict[ntype]['plas']={tab.name:tab.vector for tab in plastabset[ntype]}
     return tab_dict
 
 def multi_main(syntype,stpYN):
     from multiprocessing.pool import Pool
     p = Pool(12,maxtasksperchild=1)
     # Apply main simulation varying cortical fractions:
-    stimfreqs=[10,20,40]
+    stimfreqs=[5,10,20,40]
     params=[(freq,syntype,stpYN) for freq in stimfreqs]
     results = p.map(moose_main,params)
     return dict(zip(stimfreqs,results))
@@ -119,27 +96,29 @@ if __name__ == "__main__":
     axis=fig.axes
     axis[-1].set_xlabel('time, sec')
     for stimfreq,tabset in results.items():
-        dt=tabset['dt']['tabdt']
-        print('*********** freq',stimfreq,'tt',tabset['tt'])
-        numpts=len(tabset['plas'])
-        time=np.arange(0,dt*numpts,dt)
-        if stpYN:
-            if 'dep' in tabset.keys():
-                axis[0].plot(time[0:numpts],tabset['dep'],label='dep '+str(stimfreq))
-            if 'fac' in tabset.keys():
-                axis[0].plot(time[0:numpts],tabset['fac'],label='fac '+str(stimfreq))
-            axis[0].set_ylabel('dep or fac')
-            axis[1].plot(time[0:numpts],tabset['plas'],label='freq='+str(stimfreq))
-            axis[1].set_ylabel('plas')
-        axis[2].plot(time[0:numpts],tabset['syn']*1e9,label='freq='+str(stimfreq))
+        for ntype,tabs in tabset.items():
+            dt=tabs['syndt'] 
+            print('*********** freq',stimfreq,'tt',tabs['tt'])
+            numpts=len(tabs['syn'])
+            time=np.arange(0,dt*numpts,dt)
+            if stpYN:
+                for tabname,tab in tabs['plas'].items():
+                    if 'stp' in tabname:
+                        axisnum=1
+                    else:
+                        axisnum=0
+                    labl=ntype+'_'+tabname[-4:-1]+str(stimfreq)
+                    axis[axisnum].plot(time[0:numpts],tab,label=labl)
+                axis[0].set_ylabel('dep or fac')
+                axis[1].set_ylabel('plas')
+                axis[0].legend()
+        axis[2].plot(time[0:numpts],tabset[ntype]['syn']*1e9,label=ntype+' freq='+str(stimfreq))
         axis[2].set_ylabel('Gk*1e9')
     axis[2].legend()
 
 '''
 ToDo:
-make synchan a parameter/option
-Put the single tt connected to synchan and output tables into function for testing plasticity
-add those functions to network simulation
+add these functions to network simulation - possibly "reserve" the synapse from random time tables
 write tables to disk, move spike time and ISI calculation into function
 add trial number as parameter in network simulations
 calculate ISI across trials
