642189466bf0e491d9d7988ac48dd55e786b0f6d
Avrama Blackwell <kblackw1@localhost.localdomain>
2020-01-04 19:27:02 -0500

    bg_net: update connect_dict with between nuclei connections

--- moose_nerp/bg_net/__main__.py
+++ moose_nerp/bg_net/__main__.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
 
-######## GPnetSim.py ############
-"""\
-Create a network of GP neurons using dictionaries for channels, synapses, and network parameters
-
-Can use ghk for calcium permeable channels if ghkYesNo=1
-Optional calcium concentration in compartments (calcium=1)
-Optional synaptic plasticity based on calcium (plasyesno=1)
-Spines are optional (spineYesNo=1), but not allowed for network
-The graphs won't work for multiple spines per compartment
+######## bg_net/__main__.py ############
+"""
+Model of entire basal ganglia
+Loads in all neuron modules and all network modules
+1. Test/Fix connections from external spike trains - bg_net
+2. Investigate why running out of connections - with gp_net or spn1comp_net only
+3. Verify that single compartment neurons are behaving same in network as single
+4. Investigate why synaptic inputs not visible - with gp_net or spn1comp_net only
+5. Adjust synaptic strength to match data (use smal number of neurons)
 """
 from __future__ import print_function, division
 
@@ -28,18 +28,17 @@ from moose_nerp.prototypes import (calcium,
                                    tables,
                                    net_output,
                                    util)
-from moose_nerp import proto144 as model
+from moose_nerp import spn_1comp as model
 from moose_nerp import bg_net as net
 from moose_nerp.graph import net_graph, neuron_graph, spine_graph
 
 #names of additional neuron modules to import
-neuron_modules=['Npas2006','ep']
+neuron_modules=['ep_1comp','proto154_1compNoCal','Npas2005_1compNoCal','arky140_1compNoCal','fsi']
 ### By importing other modules, do not need to repeat all the information in param_net.py
-net_modules=['gp_net','ep_net']
+net_modules=['moose_nerp.gp_net','moose_nerp.ep_net', 'moose_nerp.spn1_net']
 
 #additional, optional parameter overrides specified from with python terminal
 model.synYN = True
-model.stpYN = True
 net.single=False
 
 create_model_sim.setupOptions(model)
@@ -53,12 +52,11 @@ param_sim.simtime=0.05
 model=create_model_sim.setupNeurons(model,network=True)
 
 #create dictionary of BufferCapacityDensity - only needed if hsolve, simple calcium dynamics
-if param_sim.hsolve and model.calYN:
-    buf_cap={neur:model.param_ca_plas.BufferCapacityDensity for neur in model.neurons.keys()}
+buf_cap={neur:model.param_ca_plas.BufferCapacityDensity for neur in model.neurons.keys()}
 
 #import additional neuron modules, add them to neurons and synapses
 for neur_module in neuron_modules:
-    neur_mod=importlib.import_module(neur_module)
+    neur_mod=importlib.import_module('moose_nerp.'+neur_module)
     #probably a good idea to give synapses to all neurons (or no neurons)
     neur_mod.synYN = model.synYN
     neur_mod.param_cond.neurontypes = util.neurontypes(neur_mod.param_cond)
