abd93cb33f513d3f336554aea8984ca07dec8a30
Avrama Blackwell <avrama@gmu.edu>
2020-01-24 13:56:09 -0500

    net_sim_graph: allow additional pulse generator

--- moose_nerp/prototypes/net_sim_graph.py
+++ moose_nerp/prototypes/net_sim_graph.py
@@ -3,16 +3,23 @@ from moose_nerp.prototypes import (create_model_sim,
                                    net_sim_graph)
 from moose_nerp.graph import net_graph, neuron_graph, spine_graph
 
-def sim_plot(model,net,connections,population):
+def sim_plot(model,net,connections,population,pg=None):
     traces, names = [], []
     for inj in model.param_sim.injection_current:
-        create_model_sim.runOneSim(model, simtime=model.param_sim.simtime, injection_current=inj)
+        print('ready to simulation with', inj)
+        if pg is None:
+            create_model_sim.runOneSim(model, simtime=model.param_sim.simtime, injection_current=inj)
+        else:
+            pg.firstLevel = inj
+            create_model_sim.runOneSim(model, simtime=model.param_sim.simtime)
         if net.single and len(model.vmtab):
             for neurnum,neurtype in enumerate(model.neurons.keys()):
                 traces.append(model.vmtab[neurtype][0].vector)
                 names.append('{} @ {}'.format(neurtype, inj))
             if model.synYN:
                 net_graph.syn_graph(connections, model.syntab, model.param_sim)
+                if model.stpYN and len(model.stp_tab):
+                    net_graph.syn_graph(connections, model.stp_tab,param_sim,graph_title='short term plasticity')
             if model.spineYN:
                 spine_graph.spineFig(model,model.spinecatab,model.spinevmtab, model.param_sim.simtime)
         else:
@@ -20,7 +27,7 @@ def sim_plot(model,net,connections,population):
                 net_graph.graphs(population['pop'], model.param_sim.simtime, model.vmtab,model.catab,model.plastab)
             if model.synYN and model.param_sim.plot_synapse:
                 net_graph.syn_graph(connections, model.syntab, param_sim)
-                if model.stpYN:
+                if model.stpYN and len(model.stp_tab):
                     net_graph.syn_graph(connections, model.stp_tab,model.param_sim,graph_title='stp',factor=1)
             #net_output.writeOutput(model, net.outfile,model.spiketab,model.vmtab,population)
 
