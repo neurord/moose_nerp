1143a5cbbc8d4c1f058105d9714a73b76e3368bf
Avrama Blackwell <avrama@gmu.edu>
2020-02-07 18:01:02 -0500

    network_plots updated to plot multiple output files

--- moose_nerp/network_plots.py
+++ moose_nerp/network_plots.py
@@ -9,43 +9,63 @@ plt.ion()
 colors=plt.get_cmap('viridis')
 
 numbins=100 #subdivide time into bins to calculate spike frequency
-fname='bg_net/output/bg_osc' #output spikes
+files=['bg_net/output/bg_osc'] #list of files with output spikes
 inputs=['bg_net/Ctx10000_osc_freq10.0_osc0.7','bg_net/STN2000_lognorm_freq18.0'] #input spikes
 max_rasters=1000
+save_txt=False
 
-dat= np.load(fname+'.npz','r',allow_pickle=True)
-spiketime_dict=dat['spike_time'].item()
-max_time=dat['params'].item()['simtime']
-binsize=max_time/numbins
-bins=[i*binsize for i in range(numbins)]
+def simple_raster(spikes,max_time,max_rasters=np.inf):
+    fig,axes =plt.subplots(len(spikes), 1,sharex=True)
+    fig.suptitle('input raster ')
+    axis=fig.axes
+    for ax,(key,spikeset) in enumerate(spikes.items()):
+        numtrains=max_rasters if len(spikeset)>max_rasters else len(spikeset)
+        color_num=[pu.colornum(cellnum,spikeset) for cellnum in range(numtrains)]
+        color_set=np.array([colors.__call__(color) for color in color_num])
+        axis[ax].eventplot(spikeset[0:numtrains],color=color_set)
+        axis[ax].set_ylabel(key)
+    axis[-1].set_xlim([0,max_time])
+    axis[-1].set_xlabel('time (s)')
 
-spike_rate={k:np.zeros(numbins) for k in spiketime_dict.keys()}
-fft_wave={}
+all_spike_rate={}
+for fname in files:
+    dat= np.load(fname+'.npz','r',allow_pickle=True)
+    spiketime_dict=dat['spike_time'].item()
+    max_time=dat['params'].item()['simtime']
+    simple_raster(spiketime_dict,max_time) #plot rasters of inputs
+    binsize=max_time/numbins
+    bins=[i*binsize for i in range(numbins)]
+    spike_rate={k:np.zeros(numbins) for k in spiketime_dict.keys()}
+    for ntype in spiketime_dict.keys():
+        numneurs=len(spiketime_dict[ntype])
+        time_hist,tmp=np.histogram(na.flatten(spiketime_dict[ntype]),bins=bins)
+        spike_rate[ntype]=time_hist/numneurs
+    all_spike_rate[os.path.basename(fname)]=spike_rate
 
-for ntype in spiketime_dict.keys():
-    flat_spikes=na.flatten(spiketime_dict[ntype])
-    numneurs=len(spiketime_dict[ntype])
-    for i,sbin in enumerate(bins):
-        spike_rate[ntype][i]=np.sum([st for st in flat_spikes if st>sbin and st<sbin+binsize])/numneurs/binsize
-    plt.plot(bins,spike_rate[ntype],label=ntype)
+#plot spike rate for each neuron type and each input file
+plot_bins=[(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
+fig,axes =plt.subplots(len(files), 1,sharex=True)
+fig.suptitle(+' rate')
+axis=fig.axes
+for i,(cond,spike_rate) in enumerate(all_spike_rate.items()):
+    axis[i].plot(plot_bins,spike_rate[ntype],label=ntype)
+    axis[ax].set_ylabel(cond, 'Hz')
+axis[-1].set_xlabel('time (sec)')
+axis[-1].set_xlim(0.0,max_time)
 plt.legend()
 
+#plot raster of input spikes-update to work with different sets of inputs, e.g. slow and fast ramp
 pre_spikes={}
 for f in inputs:
     pre_spikes[os.path.basename(f)]=np.load(f+'.npz','r',allow_pickle=True)['spikeTime']
+simple_raster(pre_spikes)
 
-fig,axes =plt.subplots(len(inputs), 1,sharex=True)
-fig.suptitle('input raster ')
-axis=fig.axes
-for ax,(key,spikeset) in enumerate(pre_spikes.items()):
-    numtrains=max_rasters if len(spikeset)>max_rasters else len(spikeset)
-    color_num=[pu.colornum(cellnum,spikeset) for cellnum in range(numtrains)]
-    color_set=np.array([colors.__call__(color) for color in color_num])
-    axis[ax].eventplot(spikeset[0:numtrains],color=color_set)
-    axis[ax].set_ylabel(key)
-axis[-1].set_xlim([0,max_time])
-axis[-1].set_xlabel('time (s)')
-
+if save_txt:
+    for cond in all_spike_rate.keys():
+        outfname=cond+'hist.txt'
+        header='time   '+'   '.join(all_spike_rate[cond].keys())
+        matrix=np.concatenate([np.array(vals)[:,None] for vals in all_spike_rate[cond].values()],axis=1)
+        np.savetxt(outfname,np.column_stack((plot_bins,matrix)),header=header)
 
 connect=dat['params'].item()['connect_dict']
 #for line in connect:
