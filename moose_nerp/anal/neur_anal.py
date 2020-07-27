3967320d35c4406a871f5d256750a04635b370f2
Avrama Blackwell <avrama@gmu.edu>
2020-07-27 16:01:14 -0400

    neur_anal: calc psp from txt, write output

--- moose_nerp/anal/neur_anal.py
+++ moose_nerp/anal/neur_anal.py
@@ -2,29 +2,32 @@ import numpy as np
 import glob
 import neur_anal_class as nac
 import plot_utils as pu
+import net_anal_utils as nau
 
-'''
 filedir='/home/avrama/moose/moose_nerp/moose_nerp/ep/output/'
 file_root='epPSP_'
 param1=['str','GPe']
-param2=[{'_freq': '10'},{'_freq': '20'},{'_freq': '40'}]#{'_freq': '5'},
-param3=[{'_plas':'0','_inj':'-1.5e-11'},{'_plas':'1','_inj':'-1.5e-11'}]
+param2=[{'_freq': '10'},{'_freq': '20'},{'_freq': '40'},{'_freq': '5'}]#,{'_freq': '1'}]
+param3=[{'_plas':'1','_inj':'-1e-11'}]
 vmtab_index=0
-start_stim=0.2
-end_stim=0.6
+start_stim=2.0#0.2
+end_stim=2.0
+transient=0.1
+plot_cal=False
+plot_traces=False
+cal_measure='mean'
+write_output=True
 '''
 #for calcium outputs
 start_stim=1.0
-transient=0.1
 filedir='/home/avrama/moose/moose_nerp/moose_nerp/ep/'
 file_root='epAP_'
 param1=['none']
 param2=[{'_freq': '10'},{'_freq': '20'},{'_freq': '40'},{'_freq': '50'}]
 param3=[{'_plas':'0','_inj':'0.00'}]
 plot_cal=False
-start_stim=1.0
 end_stim=4.0
-measure='mean'
+'''
 '''
 to do:
 1. cross_corr
@@ -41,45 +44,70 @@ for parset in param3: #generate new fig each time through this loop
             suffix=''.join([str(k)+str(v) for k,v in parset.items()])
             pattern=filedir+file_root+cond+key+suffix
             ################ npz files, with Vm, and possibly spiketimes and ISIs, synaptic stim time
-            files=sorted(glob.glob(pattern+'*.npz'))
-            if len(files)==0:
-                print('************ no npz files found using',pattern)
-            elif len(files)>1:
+            nfiles=sorted(glob.glob(pattern+'*.npz'))
+            if len(nfiles)==0:
+                print('******* no npz files found using',pattern)
+                vfiles=sorted(glob.glob(pattern+'*Vm.txt'))
+                if len(vfiles):
+                    print('----->analyzing txt files')
+                    ## Extract parameters from file name
+                    if '_inj' in parset.keys():
+                        inj=parset['_inj']
+                    elif '_inj' in params.keys():
+                        inj=params['_inj']
+                    else:
+                        inj=0
+                    if '_freq' in params.keys():
+                        freq=params['_freq']
+                    else:
+                        freq=0
+                    ### store data in structure similar to calcium
+                    data=nac.neur_text(vfiles[0])
+                    data.spikes(inj)
+                    data.psp_amp(start_stim,freq)
+                    data_set.add_data(data,cond,key)
+                    if plot_traces:
+                        pu.plot_dict(data.traces,data.time,ylabel='Vm (V)',ftitle=suffix+key)
+                else:
+                    print('************ !!!!!!!!!!! no files found using',pattern)
+                   
+            elif len(nfiles)>1:
                 print('***** multiple npz files found using',pattern)
             else:
-                fname=files[0]
+                fname=nfiles[0]
                 ##### create neur_output object for each file, add to data_set
-                data=nac.neur_output(fname)
+                data=nac.neur_npz(fname)
                 if len(data.isis):
                     plot_isis=True
                 data.psp_amp()
                 data_set.add_data(data,cond,key)
             ################ text files with calcium concentration
-            files=sorted(glob.glob(pattern+'Ca.txt'))
-            if len(files)==0:
-                print('************ no txt files found using',pattern)
-            elif len(files)>1:
+            cfiles=sorted(glob.glob(pattern+'Ca.txt'))
+            if len(cfiles)==0:
+                print('************ no txt files for calcium found using',pattern+'Ca.txt')
+            elif len(cfiles)>1:
                 print('***** multiple txt files found using',pattern+'Ca.txt')
             else:
-                fname=files[0]
-                cdata=nac.neur_calcium(fname)
+                fname=cfiles[0]
+                cdata=nac.neur_text(fname)
                 cdata.cal_stats(start_stim,transient)
                 cal_summary[key+suffix]=cdata.cal_min_max_mn
                 if plot_cal:
-                    pu.plot_dict(cdata.calcium,cdata.time,ylabel='Calcium (uM)',ftitle=suffix+key)
-    data_set.freq_inj_curve(start_stim,end_stim)
-    print([(data_set.spike_freq[p], data_set.inst_spike_freq[p]) for p in data_set.inst_spike_freq.keys()])
+                    pu.plot_dict(cdata.traces,cdata.time,ylabel='Calcium (uM)',ftitle=suffix+key)
+    if end_stim>start_stim:
+        data_set.freq_inj_curve(start_stim,end_stim)
+        print([(data_set.spike_freq[p], data_set.inst_spike_freq[p]) for p in data_set.inst_spike_freq.keys()])
     if 'cdata' in globals():
         print('CALCIUM for ',cond)
         for k,v in cal_summary.items():
-            line=sorted({comp:v[comp][measure] for comp in v}.items(),key=lambda x:len(x[0]))
+            line=sorted({comp:v[comp][cal_measure] for comp in v}.items(),key=lambda x:len(x[0]))
             print('    ',k[1:],line[0:4])
         columns=sorted(list(cdata.column_map.keys()),key=lambda x:len(x))
         soma_index=columns.index(cdata.soma_name)
         columns.insert(soma_index-1,columns.pop(soma_index))
         xdata=[float(list(p.values())[0]) for p in param2]
-        cal_mean={col:[cal_summary[p][col][measure] for p in cal_summary.keys()] for col in columns}
-        pu.plot_dict(cal_mean,xdata,xlabel='Freq (Hz)',ftitle='calcium '+measure)
+        cal_mean={col:[cal_summary[p][col][cal_measure] for p in cal_summary.keys()] for col in columns}
+        pu.plot_dict(cal_mean,xdata,xlabel='Freq (Hz)',ftitle='calcium '+cal_measure)
     else:
         print('No CALCIUM files found for', cond)
 #################### plot the set of results from single neuron simulations, range of input frequencies
@@ -87,8 +115,33 @@ for parset in param3: #generate new fig each time through this loop
         if len(data_set.psp_amp[cond]):
             #pu.plot_freq_dep(data_set.psp_amp,data_set.stim_tt,'PSP amp (mV)',suffix,len(data_set.neurtypes),xlabel='stim number',scale=1000,offset=0.01)
             pu.plot_freq_dep(data_set.psp_norm,data_set.stim_tt,'norm PSP amp',suffix,len(data_set.neurtypes),xlabel='stim number',offset=0.02)
-        if len(data_set.vmdat[cond]):
-            pu.plot_freq_dep(data_set.vmdat,data_set.time,'Vm (mV)',suffix,len(data_set.neurtypes),scale=1000,offset=0.1)
+        if len(data_set.traces[cond]):
+            pu.plot_freq_dep(data_set.traces,data_set.time,'Vm (mV)',suffix,len(data_set.neurtypes),scale=1000,offset=0.1)
         if plot_isis:
             pu.plot_freq_dep(data_set.isis,data_set.isi_x,'ISI (sec)',suffix,len(data_set.neurtypes),xlabel='Spike time (sec)')
+    else:
+        pu.plot_dict_of_dicts(data_set.psp_amp,ylabel='PSP amp',xlabel='stim number',ftitle=suffix)
+        pu.plot_dict_of_dicts(data_set.psp_norm,ylabel='NORM PSP amp',xlabel='stim number',ftitle=suffix)
+    if write_output:
+        #transpose dictionaries
+        import pandas as pd
+        dfy=pd.DataFrame(data_set.psp_norm)
+        output_dict=dfy.transpose().to_dict()
+        dfx=pd.DataFrame(data_set.stim_tt)
+        xvals=dfx.transpose().to_dict()
+        for k1 in output_dict.keys():
+            fname='psp_norm_'+k1
+            header='  '.join(['psp_norm_'+k2+'_'+k1 for k2 in output_dict[k1].keys() ])
+            outputdata=nau.choose_xvals(xvals[k1])
+            for k2 in output_dict[k1].keys():
+                outputdata=np.column_stack((outputdata,output_dict[k1][k2]['ep']))
+            f=open(fname+".txt",'w')
+            f.write(header+'\n')
+            np.savetxt(f,outputdata,fmt='%.5f')
+            f.close()
+        if 'spike_freq' in data_set.__dict__:
+            nau.write_dict_of_dicts(data_set.spike_freq,'fI'+suffix,'freq'+suffix)
+        if 'cal_mean' in globals():
+            nau.write_dict_of_dicts(cal_mean,xdata,'calcium'+suffix,'calcium'+suffix)
+
 
--- moose_nerp/anal/neur_anal_class.py
+++ moose_nerp/anal/neur_anal_class.py
@@ -1,6 +1,7 @@
 import numpy as np
+mM_to_uM=1000
 
-class neur_output():
+class neur_npz():
     def __init__(self,fname,table_index=None):
         dat=np.load(fname,'r',allow_pickle=True)
         self.fname=fname
@@ -11,8 +12,8 @@ class neur_output():
             print('no spike times or isi in', fname)
             self.isis={}
             self.spiketime={neur:[] for neur in dat['vm'].item()}
-        self.vmdat=dat['vm'].item()
-        self.neurtypes=list(self.vmdat.keys())
+        self.traces=dat['vm'].item()
+        self.neurtypes=list(self.traces.keys())
         if table_index:
             self.somatab=table_index
         else:
@@ -25,8 +26,8 @@ class neur_output():
         if 'dt' in dat['params'].item():
             self.dt=dat['params'].item()['dt']
         else:
-            self.dt=self.sim_time/len(self.vmdat[neur][0])
-        self.time=np.linspace(0,self.dt*len(self.vmdat[neur][0]),len(self.vmdat[neur][0]),endpoint=False)
+            self.dt=self.sim_time/len(self.traces[neur][0])
+        self.time=np.linspace(0,self.dt*len(self.traces[neur][0]),len(self.traces[neur][0]),endpoint=False)
         for neur in self.neurtypes:
             if 'syn_tt' in dat['params'].item().keys():
                 ############### Preferred, but new way, of storing regular time-table data for ep simulations #######
@@ -42,16 +43,21 @@ class neur_output():
                 xend=stim_tt[-1]
                 self.syntt_info[neur]={'xstart':xstart,'xend':xend,'stim_tt':stim_tt,'loc':stim_loc}
 
-    def psp_amp(self):
-        self.psp_amp={neur:[] for neur in self.vmdat.keys()}
-        self.psp_norm={neur:[] for neur in self.vmdat.keys()}
-        for ntype in self.vmdat.keys():
+    def psp_amp(self,peak='min'):
+        self.psp_amp={neur:[] for neur in self.traces.keys()}
+        self.psp_norm={neur:[] for neur in self.traces.keys()}
+        for ntype in self.traces.keys():
             if len(self.spiketime[ntype])==0:
-                vmtab=self.vmdat[ntype][self.somatab]
+                vmtab=self.traces[ntype][self.somatab]
                 stim_tt=self.syntt_info[ntype]['stim_tt']
                 vm_init=[vmtab[int(t/self.dt)] for t in stim_tt] #find Vm at the time of stim
-                vm_peak=[np.min(vmtab[int(t/self.dt):int((t+1/self.freq)/self.dt)]) for t in stim_tt] #find peak between pair of stims
-                self.psp_amp[ntype]=np.array(vm_init)-np.array(vm_peak)  #calculate diff between init and peak
+                if peak=='min':
+                    vm_peak=[np.min(vmtab[int(t/self.dt):int((t+1/self.freq)/self.dt)]) for t in stim_tt] #find peak between pair of stims
+                elif peak=='max':
+                    vm_peak=[np.mmax(vmtab[int(t/self.dt):int((t+1/self.freq)/self.dt)]) for t in stim_tt]
+                else:
+                    print('!!!!!!!!!! specify min (for IPSPs) or max (for EPSPs)')
+                self.psp_amp[ntype]=np.array(vm_peak)-np.array(vm_init)  #calculate diff between init and peak
                 self.psp_norm[ntype]=[amp/self.psp_amp[ntype][0] for amp in self.psp_amp[ntype]] #normalize to 1st one in train
             else:
                 print('psp not calculated, spikes found')
@@ -60,7 +66,7 @@ class neur_set():
     def __init__(self,param1):
         self.psp_norm={c:{} for c in param1}
         self.psp_amp={c:{} for c in param1}
-        self.vmdat={c:{} for c in param1}
+        self.traces={c:{} for c in param1}
         self.isis={c:{} for c in param1}
         self.stim_tt={c:{} for c in param1}
         self.time={c:{} for c in param1}
@@ -68,15 +74,24 @@ class neur_set():
         self.spiketime={c:{} for c in param1}
         
     def add_data(self,data,par1,key):
-        self.spiketime[par1][key]=data.spiketime
-        self.psp_norm[par1][key]=data.psp_norm
-        self.psp_amp[par1][key]=data.psp_amp
-        self.vmdat[par1][key]=data.vmdat
-        self.isis[par1][key]=data.isis
-        self.time[par1][key]={neur:data.time for neur in data.syntt_info}
-        self.stim_tt[par1][key]={neur:range(len(data.syntt_info[neur]['stim_tt'])) for neur in data.syntt_info}
-        self.neurtypes=data.neurtypes
-        self.params[par1][key]={'freq':data.freq,'inj':data.inj}
+        if key.startswith('_'):
+            newkey=key[1:]
+        else:
+            newkey=key
+        self.spiketime[par1][newkey]=data.spiketime
+        self.psp_norm[par1][newkey]=data.psp_norm
+        self.psp_amp[par1][newkey]=data.psp_amp
+        self.traces[par1][newkey]=data.traces
+        if 'isis' in data.__dict__:
+            self.isis[par1][newkey]=data.isis
+        if 'neurtypes' in data.__dict__:
+            self.neurtypes=data.neurtypes
+            self.time[par1][newkey]={neur:data.time for neur in data.syntt_info}
+            self.stim_tt[par1][newkey]={neur:range(len(data.syntt_info[neur]['stim_tt'])) for neur in data.syntt_info}
+        else:
+            self.time[par1][newkey]=data.time
+            self.stim_tt[par1][newkey]=data.syntt_info
+        self.params[par1][newkey]={'freq':data.freq,'inj':data.inj}
 
     def freq_inj_curve(self,start,end):
         stim_spikes={c:{} for c in self.spiketime.keys()} 
@@ -85,15 +100,15 @@ class neur_set():
         self.spike_freq={c:{} for c in self.spiketime.keys()}
         for p in self.spiketime.keys():
             for k in self.spiketime[p].keys():
-                stim_spikes[p][k]={n:[st for st in spikeset if st > start_stim and st < end_stim]
-                                   for n,spikeset in self.spiketime[p][k].items()}
-                self.spike_freq[p][k]={n:len(spikes)/(end-start) for n,spikes in stim_spikes[p][k].items()}
-                self.stim_isis[p][k]={n:[spikeset[i+1]-spikeset[i] for i in range(len(spikeset)-1)]
-                                    for n,spikeset in self.spiketime[p][k].items()}
-                self.inst_spike_freq[p][k]={n:[np.round(np.mean([1/isi for isi in isiset]),2)] for n,isiset in self.stim_isis[p][k].items()}
-mM_to_uM=1000
+                if len(self.spiketime[p][k]):
+                    stim_spikes[p][k]={n:[st for st in spikeset if st > start_stim and st < end_stim]
+                                       for n,spikeset in self.spiketime[p][k].items()}
+                    self.spike_freq[p][k]={n:len(spikes)/(end-start) for n,spikes in stim_spikes[p][k].items()}
+                    self.stim_isis[p][k]={n:[spikeset[i+1]-spikeset[i] for i in range(len(spikeset)-1)]
+                                        for n,spikeset in self.spiketime[p][k].items()}
+                    self.inst_spike_freq[p][k]={n:[np.round(np.mean([1/isi for isi in isiset]),2)] for n,isiset in self.stim_isis[p][k].items()}
 
-class neur_calcium():
+class neur_text():
     def __init__(self,fname,soma='soma'):
         self.column_dict={}
         with open(fname) as fo:
@@ -101,14 +116,42 @@ class neur_calcium():
             time_index=header.index('time')-1  #-1 here and column_dict because header begins with '# time'
             columns=[head.split('/')[-2]+head[2:].split('/')[-1] for head in header[2:]]
             self.column_map={col:i+1 for i, col in enumerate(columns)}
-            self.soma_name=[col for col in columns if soma in col][0] 
+            self.soma_name=[col for col in columns if soma in col][0]
         dat=np.loadtxt(fname)
         self.time=dat[:,time_index]
-        self.calcium={col:mM_to_uM*dat[:,i+1] for i,col in enumerate(self.column_map.keys())}
+        self.traces={col:mM_to_uM*dat[:,i+1] for i,col in enumerate(self.column_map.keys())}
+        self.dt=self.time[1]
+        self.sim_time=self.time[-1]
+        
     def cal_stats(self,start_stim,settle_time):
         start_search=np.min(np.where(self.time>start_stim+settle_time)) 
-        self.cal_min_max_mn={comp:{'max':np.max(cal[start_search:]),'min':np.min(cal[start_search:])} for comp,cal in self.calcium.items()}
-        for comp,cal in self.calcium.items():
+        self.cal_min_max_mn={comp:{'max':np.max(cal[start_search:]),'min':np.min(cal[start_search:])} for comp,cal in self.traces.items()}
+        for comp,cal in self.traces.items():
             self.cal_min_max_mn[comp]['mean_trace']=np.round(np.mean(cal[start_search:]),4) 
             self.cal_min_max_mn[comp]['mean']=np.round((self.cal_min_max_mn[comp]['min']+self.cal_min_max_mn[comp]['max'])/2,4) 
 
+    def spikes(self,inj):
+        import detect
+        self.spiketime=[]
+        self.inj=inj
+        vmtab=self.traces[self.soma_name]
+        self.spiketime.append(detect.detect_peaks(vmtab)*self.dt)
+            
+    def psp_amp(self,start_stim,freq,peak='min'):
+        self.freq=int(freq)
+        self.psp_amp={neur:[] for neur in self.traces.keys()}
+        self.psp_norm={neur:[] for neur in self.traces.keys()}
+        vmtab=self.traces[self.soma_name]
+        stim_tt=[start_stim+float(n)/self.freq for n in range(self.freq)]
+        vm_init=[vmtab[int(t/self.dt)] for t in stim_tt] #find Vm at the time of stim
+        if peak=='min':
+            vm_peak=[np.min(vmtab[int(t/self.dt):int((t+1/self.freq)/self.dt)]) for t in stim_tt] #find peak between pair of stims
+        elif peak=='max':
+            vm_peak=[np.max(vmtab[int(t/self.dt):int((t+1/self.freq)/self.dt)]) for t in stim_tt] #
+        else:
+            print('!!!!!!!!!! specify min (for IPSPs) or max (for EPSPs)')
+        self.psp_amp=np.array(vm_peak)-np.array(vm_init)  #calculate diff between init and peak
+        self.psp_norm=[amp/self.psp_amp[0] for amp in self.psp_amp] #normalize to 1st in train
+        xstart=stim_tt[0]
+        xend=stim_tt[-1]
+        self.syntt_info={'xstart':xstart,'xend':xend,'stim_tt':stim_tt}
