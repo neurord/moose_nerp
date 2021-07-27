7e9a099f471928445ccde253661caf08216dbb02
Avrama Blackwell <avrama@gmu.edu>
2021-07-27 12:24:10 -0400

    str_net/__main__: avoid plotting when streaming tables to disk additional dictionary returned from create_network

--- moose_nerp/str_net/__main__.py
+++ moose_nerp/str_net/__main__.py
@@ -14,6 +14,7 @@ from __future__ import print_function, division
 import logging
 
 import numpy as np
+np.random.seed(2020)
 import matplotlib.pyplot as plt
 plt.ion()
 
@@ -32,12 +33,13 @@ from moose_nerp.prototypes import (create_model_sim,
                                    logutil,
                                    util,
                                    standard_options)
-from moose_nerp import d1opt as model
+#from moose_nerp import d1opt as model
+from moose_nerp import D1MatrixSample2 as model
 from moose_nerp import str_net as net
 from moose_nerp.graph import net_graph, neuron_graph, spine_graph
 
 #names of additional neuron modules to import
-##### !!!!!!!!!!! CHANGE TO 'd2opt' WHEN THAT DIRECTORY AVAILABLE
+##### !!!!!!!!!!! CHANGE TO 'd2opt' WHEN THAT optimized D2 neuron available
 #neuron_modules=['d1d2']
 neuron_modules=[]
 
@@ -45,7 +47,7 @@ neuron_modules=[]
 #model.Condset.D1.NaF[model.param_cond.prox] /= 3
 #model.Condset.D1.KaS[model.param_cond.prox] *= 3
 net.connect_dict['D1']['ampa']['extern1'].dend_loc.postsyn_fraction = 0.7
-net.param_net.tt_Ctx_SPN.filename = 'str_net/FullTrialLowVariability'
+#net.param_net.tt_Ctx_SPN.filename = 'str_net/FullTrialLowVariability'
 model.synYN = True
 model.plasYN = True
 model.calYN = True
@@ -55,14 +57,14 @@ net.single=True
 #    model.param_ca_plas.CaShellModeDensity[k] = model.param_ca_plas.SHELL
 create_model_sim.setupOptions(model)
 param_sim = model.param_sim
-param_sim.useStreamer = False#True
+param_sim.useStreamer = True
 param_sim.plotdt = .1e-3
 param_sim.stim_loc = model.NAME_SOMA
 param_sim.stim_paradigm = 'inject'
 param_sim.injection_current = [0] #[-0.2e-9, 0.26e-9]
 param_sim.injection_delay = 0.2
 param_sim.injection_width = 0.4
-param_sim.simtime = .003#.#21
+param_sim.simtime = 1.1#.003#.#21
 net.num_inject = 0
 if net.num_inject==0:
     param_sim.injection_current=[0]
@@ -77,14 +79,12 @@ if len(neuron_modules):
     buf_cap=multi_module.multi_modules(neuron_modules,model,buf_cap)
 
 ########### Create Network. For multiple populations, send in net_modules ###########
-population,connections,plas=create_network.create_network(model, net, model.neurons)
+population,[connections,conn_summary],plas=create_network.create_network(model, net, model.neurons)
 
 ###### Set up stimulation - could be current injection or plasticity protocol
 # set num_inject=0 to avoid current injection
 if net.num_inject<np.inf :
     model.inject_pop=inject_func.inject_pop(population['pop'],net.num_inject)
-    if net.num_inject==0:
-        param_sim.injection_current=[0]
 else:
     model.inject_pop=population['pop']
 
@@ -102,7 +102,7 @@ else:   #population of neurons
     simpath=[net.netname]
 
 clocks.assign_clocks(simpath, param_sim.simdt, param_sim.plotdt, param_sim.hsolve,model.param_cond.NAME_SOMA)
-# Fix calculation of B parameter in CaConc if using hsolve
+# Fix calculation of B parameter in CaConc if using hsolve and simple calcium dynamics
 ######### Need to use CaPlasticityParams.BufferCapacityDensity from EACH neuron_module
 if model.param_sim.hsolve and model.calYN:
     calcium.fix_calcium(model.neurons.keys(), model, buf_cap)
@@ -147,18 +147,18 @@ for inj in param_sim.injection_current:
         for neurnum,neurtype in enumerate(util.neurontypes(model.param_cond)):
             traces.append(model.vmtab[neurtype][0].vector)
             names.append('{} @ {}'.format(neurtype, inj))
-        if model.synYN:
+        if model.synYN and not param_sim.useStreamer:
             net_graph.syn_graph(connections, model.syntab, param_sim)
-        if model.plasYN:
+        if model.plasYN and not param_sim.useStreamer:
             net_graph.syn_graph(connections, model.plastab, param_sim, graph_title='Plas Weight')
             net_graph.syn_graph(connections, nonstim_plastab, param_sim, graph_title='NonStim Plas Weight')
 
         if model.spineYN:
             spine_graph.spineFig(model,model.spinecatab,model.spinevmtab, param_sim.simtime)
     else:
-        if net.plot_netvm:
+        if net.plot_netvm and not param_sim.useStreamer:
             net_graph.graphs(population['pop'], param_sim.simtime, model.vmtab,model.catab,model.plastab)
-        if model.synYN and param_sim.plot_synapse:
+        if model.synYN and param_sim.plot_synapse and not param_sim.useStreamer:
             net_graph.syn_graph(connections, model.syntab, param_sim)
         net_output.writeOutput(model, net.outfile+str(inj),model.spiketab,model.vmtab,population)
 
