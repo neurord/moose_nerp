46032f91b479e87ff1356557e87d6de69eaca4bf
Avrama Blackwell <avrama@gmu.edu>
2021-07-27 12:29:09 -0400

    spine_graph: use different time vector for calcium and voltage

--- moose_nerp/graph/spine_graph.py
+++ moose_nerp/graph/spine_graph.py
@@ -7,6 +7,7 @@ def spineFig(model,spinecatab,spinevmtab,simtime):
     f=plt.figure()
     f.canvas.set_window_title('Spines')
     t = np.linspace(0, simtime, len(spinevmtab[0][0].vector))
+    tca = np.linspace(0, simtime, len(spinecatab[0][0].vector))
     if model.calYN:
         plt.subplot(211)
     for neurnum in range(len(neurontypes(model.param_cond))):
@@ -20,7 +21,7 @@ def spineFig(model,spinecatab,spinevmtab,simtime):
         for neurnum in range(len(neurontypes(model.param_cond))):
             for oid in spinecatab[neurnum]:
                 name=''.join(oid.name[oid.name.find('_')-2:].split('dend'))
-                plt.plot(t,1000*oid.vector,label=name)
+                plt.plot(tca,1000*oid.vector,label=name)
             plt.ylabel('calcium, uM')
     plt.legend()
     plt.show()
