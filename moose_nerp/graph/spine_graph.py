9621092b09d67c6f1deeea0217f741990301a00a
avrama <avrama@gmu.edu>
2021-10-21 16:59:31 -0400

    spine_graph:test for spine tables with 2 inputs (error)

--- moose_nerp/graph/spine_graph.py
+++ moose_nerp/graph/spine_graph.py
@@ -12,16 +12,22 @@ def spineFig(model,spinecatab,spinevmtab,simtime):
         plt.subplot(211)
     for neurnum in range(len(neurontypes(model.param_cond))):
         for oid in spinevmtab[neurnum]:
-            #find()-2 removes Vm from name, split and join removes dend from name
-            name = ''.join(oid.name[oid.name.find('_')-2:].split('dend'))
-            plt.plot(t,oid.vector,label=name)
-        plt.ylabel('Vm')
+            if len(oid.vector)==len(t):
+                #find()-2 removes Vm from name, split and join removes dend from name
+                name = ''.join(oid.name[oid.name.find('_')-2:].split('dend'))
+                plt.plot(t,oid.vector,label=name)
+            else:
+                print('data problem with spine Vm for', oid)
+            plt.ylabel('Vm')
     if model.calYN:
         plt.subplot(212)
         for neurnum in range(len(neurontypes(model.param_cond))):
             for oid in spinecatab[neurnum]:
-                name=''.join(oid.name[oid.name.find('_')-2:].split('dend'))
-                plt.plot(tca,1000*oid.vector,label=name)
+                if len(oid.vector)==len(t):
+                    name=''.join(oid.name[oid.name.find('_')-2:].split('dend'))
+                    plt.plot(tca,1000*oid.vector,label=name)
+            else:
+                print('data problem with spine Ca for', oid)
             plt.ylabel('calcium, uM')
     plt.legend()
     plt.show()
