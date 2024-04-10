2f5c1d939034addccaa21e83fd560cb145d2031d
avrama <kblackw1@gmu.edu>
2024-04-10 19:38:05 -0400

    sim_upstate_BLA: add loop to perform multiple sims with different seeds

--- moose_nerp/sim_upstate_BLA.py
+++ moose_nerp/sim_upstate_BLA.py
@@ -1,33 +1,24 @@
 import sys
 import numpy as np
-import argparse
 import sim_upstate as su
-
-def parsarg(commandline):
-    small_parser=argparse.ArgumentParser()
-    small_parser.add_argument('-BLA', type=str, help='clustered stim from BLA to DMS or DLS')
-    small_parser.add_argument('-tt',type=str,default='networks/FullTrialMediumVariabilitySimilarTrialsTruncatedNormal', help='spike train file')
-    small_parser.add_argument('-SPN', type=str, default="D1MatrixSample2", help='neuron module')
-    small_parser.add_argument('-seed', type=int, help='dispersed spine seed')
-
-    args=small_parser.parse_args(commandline)
-    return args
 
 mod_dict = su.make_mod_dict()
 clustered_seed = 135
 single_epsp_seed = 314
+num_sims=4
 
 #args='-BLA DMS'.split()# -seed 123'.split()
 args = sys.argv[1:]
-params=parsarg(args)
-sim_type='BLA_'+params.BLA+'_dispersed'
-####### OR, create loop, randomly select seed in loop and call upstate_main ############Ad
-if not params.seed:
-    dispersed_seed=np.random.randint(0,32000) 
-else:
-    dispersed_seed=params.seed
-sims=su.specify_sims(sim_type,clustered_seed,dispersed_seed,single_epsp_seed)
-tt_Ctx_SPN = {'fname': params.tt,'syn_per_tt':2} 
-model=su.upstate_main(params.SPN, mod_dict, **sims[0]['kwds'],do_plots=True,filename=sims[0]['name']+str(dispersed_seed), time_tables=tt_Ctx_SPN)
-print(sims[0])
+params=su.parsarg(args)
+
+for ii in range(num_sims):
+    ####### randomly select seed and call upstate_main ############Ad
+    if not params.seed:
+        dispersed_seed=np.random.randint(0,32000) 
+    else:
+        dispersed_seed=params.seed
+    sims=su.specify_sims(params.sim_type,clustered_seed,dispersed_seed,single_epsp_seed, params)
+    tt_Ctx_SPN = {'fname': params.tt,'syn_per_tt':2} 
+    model=su.upstate_main(params.SPN, mod_dict, **sims[0]['kwds'],do_plots=True,filename=sims[0]['name']+str(dispersed_seed), time_tables=tt_Ctx_SPN)
+    print(sims[0])
 
