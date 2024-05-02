44a1e1c253a58767cdb30aba7ee34837b8cd49ef
avrama <kim-blackwell@uiowa.edu>
2024-05-02 17:53:03 -0500

    __main__: Fix imports for packages in cells

--- moose_nerp/cells/D1MatrixSample3/__main__.py
+++ moose_nerp/cells/D1MatrixSample3/__main__.py
@@ -17,7 +17,7 @@ do: model.spinesYN = True (Default is set in param_model_defaults).
 
 from __future__ import print_function, division
 
-from moose_nerp import D1MatrixSample3 as model
+from moose_nerp.cells import D1MatrixSample3 as model
 '''Evaluates moose_nerp/d1d2/__init__.py to load all the parameters, e.g.
 param_sim.py, param_ca_plas.py, param_chan.py, param_cond.py, param_sim.py, etc.
 into the model namespace. These parameters are then accessible by, e.g.,
@@ -31,9 +31,9 @@ running a simulation, the output tables would be accessible as model.vmtab,
 model.catab, etc.'''
 
 # Parameter overrides can be specified:
-model.spineYN=False
+model.spineYN=True
 model.calYN=True
-model.param_sim.plot_channels = True
+model.param_sim.plot_channels = False
 
 model.CaPlasticityParams.CaShellModeDensity = {model.CaPlasticityParams.soma:model.CaPlasticityParams.SHELL, model.CaPlasticityParams.dend:model.CaPlasticityParams.SHELL, model.CaPlasticityParams.spines:model.CaPlasticityParams.SLAB}
 
